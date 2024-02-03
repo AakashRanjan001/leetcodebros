2a61197b0b3c5114642fc10e14612078a6ad2dca
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-02-03 10:02:01 +0530

    Time: 8 ms (80.53%), Space: 9.8 MB (25.76%) - LeetHub

---
+++ 2475-number-of-unequal-triplets-in-array/2475-number-of-unequal-triplets-in-array.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+public:
+    int unequalTriplets(vector<int>& nums) {
+        
+        int ans = 0;
+        for(int i = 0; i < nums.size(); ++i){
+            for(int j = i + 1; j < nums.size(); ++j){
+                for(int k = j +1; k < nums.size(); ++k){
+                    if(nums[i] != nums[j] && nums[i] != nums[k] && nums[j] != nums[k]) ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
