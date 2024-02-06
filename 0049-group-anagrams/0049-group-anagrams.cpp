4be1fde6405b0af78f058b6b292c5b102de0c098
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-02-06 17:05:39 +0530

    Time: 33 ms (46.51%), Space: 24.3 MB (25.67%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,17 +1,23 @@
 
 class Solution {
-    public:
-        vector<vector<string>> groupAnagrams(vector<string>& strs) {
-                unordered_map<string,vector<string>>mp;
-                        for(auto s:strs){
-                                    string x=s;
-                                                sort(x.begin(),x.end());
-                                                            mp[x].push_back(s);
-                                                                    }
-                                                                            vector<vector<string>>answer;
-                                                                                    for(auto x:mp){
-                                                                                                answer.push_back(x.second);
-                                                                                                        }
-                                                                                                                return answer;
-                                                                                                                    }
-                                                                                                                    };
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;                       
+        
+        unordered_map<string, vector<string>> mp;         
+
+        for(int i = 0; i < strs.size(); i++) {          
+            
+            string s = strs[i];                         
+            
+            sort(strs[i].begin(), strs[i].end());      
+            
+            mp[strs[i]].push_back(s);                 
+            
+        }
+        for(auto i : mp)                         
+            ans.push_back(i.second);
+        
+        return ans;      
+    }
+};
