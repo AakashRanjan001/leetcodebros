83dc2182eef3f219828f14c00fdf1c584f28cfae
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-05-04 11:00:03 +0530

    Time: 59 ms (47.36%), Space: 45.5 MB (46.86%) - LeetHub

---
+++ 0917-boats-to-save-people/0917-boats-to-save-people.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+
+        int i=0;
+        int j= people.size()-1;
+        int cnt =0;
+        while(i<=j){
+            if(people[i]+people[j]<=limit){
+                i++;
+                j--;
+                cnt++;
+            }
+            else if(people[i]+people[j]>limit){
+                cnt++;
+                j--;
+            }
+        }
+        return cnt;
+    }
+};
