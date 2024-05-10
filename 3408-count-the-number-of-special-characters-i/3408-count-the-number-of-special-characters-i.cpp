dd073cf901f537b5f24e13978cd78d693988eaae
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-05-10 10:33:32 +0530

    Time: 0 ms (100%), Space: 8.9 MB (41.51%) - LeetHub

--- 3408-count-the-number-of-special-characters-i/3408-count-the-number-of-special-characters-i.cpp
+++ 3408-count-the-number-of-special-characters-i/3408-count-the-number-of-special-characters-i.cpp
@@ -1,29 +1,24 @@
-class Solution
-{
-    public:
-        int numberOfSpecialChars(string word)
-        {
-            vector<int> smallChar(26, 0);
-            vector<int> capitalChar(26, 0);
-            int count = 0;
-            for (char ch: word)
-            {
-                if (ch >= 'a' && ch <= 'z')
-                {
-                    smallChar[ch - 'a'] = 1;
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        unordered_map<char,bool>mpp;
+        int cnt =0;
+        
+        for(int i=0;i<word.size();i++){
+            if(word[i]>=97 && word[i]<=122 && mpp[word[i]]!=true){
+                mpp[word[i]]=true;
+                
+                char c1 = word[i];
+                char c2 = toupper(c1);
+                for(auto j:word){
+                    if(j==c2){
+                        cnt++;
+                        break;
+                    }
                 }
-                else
-                {
-                    capitalChar[ch - 'A'] = 1;
-                }
+
             }
-            for (int i = 0; i < 26; i++)
-            {
-                if (smallChar[i] == 1 && capitalChar[i] == 1)
-                {
-                    count++;
-                }
-            }
-            return count;
         }
+        return cnt;
+    }
 };
