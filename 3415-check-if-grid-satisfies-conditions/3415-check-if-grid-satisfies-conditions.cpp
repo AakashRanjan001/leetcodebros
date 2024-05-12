286e3ba70eaa105c0bf740c00305e064b3ecdac0
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-05-12 10:21:11 +0530

    Time: 8 ms (66.67%), Space: 28.6 MB (50%) - LeetHub

---
+++ 3415-check-if-grid-satisfies-conditions/3415-check-if-grid-satisfies-conditions.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool satisfiesConditions(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        for (int i = 0; i + 1 < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] != grid[i+1][j]) {
+                    return false;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j + 1 < n; j++) {
+                if (grid[i][j] == grid[i][j+1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
