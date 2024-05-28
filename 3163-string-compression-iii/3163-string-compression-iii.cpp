991d8f4548225b9e052d4913c34d870f13a4e850
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-05-28 11:58:23 +0530

    Time: 64 ms (98.46%), Space: 27.6 MB (81.73%) - LeetHub

---
+++ 3163-string-compression-iii/3163-string-compression-iii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string compressedString(string word) {
+        string comp ="";
+        int n = word.length();
+        for(int i =0;i<n;i++){
+            int cnt = 1;
+            while(i<n && word[i]==word[i+1]){
+                  cnt++;
+                  i++;
+
+                     if(cnt == 9){break;
+                     }
+            }
+           comp+=char(cnt + '0');
+         comp.push_back(word[i]);
+        }
+       return comp;
+
+    }
+};
