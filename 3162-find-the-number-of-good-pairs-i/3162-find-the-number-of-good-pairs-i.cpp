681bf26991b8b3cdad46f1a0840e91825db1d28e
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-05-28 10:53:53 +0530

    Time: 10 ms (40.73%), Space: 40.9 MB (96.53%) - LeetHub

---
+++ 3162-find-the-number-of-good-pairs-i/3162-find-the-number-of-good-pairs-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        
+        int good_pairs =0;
+        for(int i=0;i<nums1.size();i++){
+            for(int j=0;j<nums2.size();j++){
+                if( nums1[i]%(nums2[j]*k)==0){
+                    good_pairs++;
+                }
+            }
+        }
+        return good_pairs;
+    }
+};
