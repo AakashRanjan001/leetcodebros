ec11bed39d522a1dbb592ec870c24b8fd8f8f39a
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-06-05 13:49:33 +0530

    Time: 0 ms (100%), Space: 7.7 MB (32.48%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+    
+    for(int i=0;i<s.size();i++){
+        char ch = s[i];
+
+      //openeing bracket 
+      if(ch == '(' || ch == '{' || ch == '['){
+         st.push(ch);     
+      }
+      
+
+
+      else{  //closing bracket
+      
+           if(st.empty()){
+             //closing bracket hai but stack empty hai matlab opening bracket nhi hai
+             return false;
+           }
+           
+           if(!st.empty()){
+            char topelement = st.top();//stack empty nhi hai
+            if(topelement == '(' && ch ==')'){
+                 //matching mil gy
+                 st.pop();
+           }
+           else if(topelement == '{' && ch =='}'){
+                 //matching mil gy
+                 st.pop();
+           }
+           else if(topelement == '[' && ch ==']'){
+                 //matching mil gy
+                 st.pop();
+           }
+           else{ //brackets not matching
+            return false;
+           }
+     }
+
+ }
+
+    }
+    if(st.empty()){  //loop ke bahar aa gey matlab stack empty ho gya sab pop ho gya sabka pair mil gya 
+      return true;
+    }
+    else{
+        return false;
+    }
+        
+    }
+};
