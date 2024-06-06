9c141ceb890b73fbb6f29cc535fc7b6ff2c70db3
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-06-06 13:21:07 +0530

    Time: 7 ms (24.66%), Space: 8.6 MB (65.22%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int>st; //we are pushing index so numeric
+        st.push(-1);
+        int maxi = 0;
+
+        for(int i=0;i<s.size();i++){
+            char ch = s[i];
+
+            if(ch =='('){  //opening bracket mila toh push kar denge stack mei 
+                st.push(i);
+            }
+            else{ //we got closing bracket
+               st.pop();
+               if(!st.empty()){ //clsosing bracket milne ke baad check kiya stack empty toh nhi haih 
+                  int len = i - st.top();
+                  maxi = max(len,maxi);
+               }
+               else{  // stack empty hai after we got the closing bracket
+                  st.push(i);
+
+               }
+
+            }
+        }
+        return maxi;
+    }
+};
