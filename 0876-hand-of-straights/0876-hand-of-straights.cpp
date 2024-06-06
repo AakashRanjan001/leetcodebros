8ecb750922fcb2e74a338acca13910c72837ce9e
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-06-06 11:08:49 +0530

    Time: 63 ms (47.4%), Space: 30.5 MB (78.12%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.cpp
+++ 0876-hand-of-straights/0876-hand-of-straights.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    bool isNStraightHand(vector<int>& hand, int gz) {
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
         int n = hand.size();
-        if(n % gz)return false;
-        sort(hand.begin(), hand.end());
-        map <int,int> mp;
-        for(int i : hand)mp[i]++;
-        for(int num : hand){
-            if(mp[num] == 0)continue;
-            int cnt = mp[num];
-            for(int j = 0; j < gz; j++){
-                if(mp[num+j] >= cnt){
-                    mp[num+j] -= cnt;
-                }
-                else return false;
-            }
+        map<int,int>mp;
+         for(auto i: hand){
+        mp[i]++;
+    }
+  if(n%groupSize!=0)return false;
+   for(auto [h,freq]:mp){
+    if(freq==0)continue;
+    while(freq--){
+        for(int j=h;j<h+groupSize;j++){
+            if(mp[j]==0)return false;
+            mp[j]--;
         }
-        return true;
+    }
+
+ }
+ return true;
+
     }
 };
