9173237c85d79e2c156613669f51fd55b6a2814b
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-06-12 11:33:03 +0530

    Time: 99 ms (69.3%), Space: 130.5 MB (46.86%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        
+        int deficit =0;
+        //petrol kitna kam pad gya ki aage nhi badh apaaye
+        int balance =0;
+        //extra petrol we have 
+        int start =0;
+        //we started from 0th index for checkung the circular route 
+
+        for(int i=0;i<gas.size();i++){
+
+            balance = gas[i]-cost[i]+balance; //extra petrol kitna bacha 
+            if(balance<0){//matlab aage nhi badh paayenge 
+
+            //maine deficit = balance kiya tha dhyan dena yaha par 2nd test case ko dekhna samjh aa kayega 
+              deficit += balance;//jo balance hoga utna hi deficit hoga -ve mei
+              start = i+1;//so aage wale index se check akrenge 
+              balance =0;//wapsa se balance ko 0 kar denge 
+
+            }
+        }
+        if(balance+deficit>=0){
+            return start; // janb pura execute ho gya tab check kiye ki balance kitna hai aur bada hai hence circle complete ho jayega 
+        }else{
+            return -1;
+        }
+    
+    }
+};
