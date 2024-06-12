94f23dbdcbaf8c912a5310da87dc04d67c90a531
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-06-12 17:30:08 +0530

    Time: 3 ms (38.8%), Space: 8.5 MB (56.19%) - LeetHub

---
+++ 3029-minimum-time-to-revert-word-to-initial-state-i/3029-minimum-time-to-revert-word-to-initial-state-i.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumTimeToInitialState(string word, int k) {
+        int n = word.length();
+
+        for(int i=k;i<n;i+=k){
+            if(word.substr(0,n-i)==word.substr(i,n-i)){
+                return i/k;
+            }
+        }
+        return (n+k-1)/k;
+
+    }
+};
