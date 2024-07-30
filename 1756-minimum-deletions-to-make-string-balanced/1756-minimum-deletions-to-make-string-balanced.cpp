ea810bce9f11aeafe84c2f6381688c4052fb0f37
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-07-30 07:58:40 +0530

    Time: 87 ms (65.07%), Space: 24.7 MB (51.04%) - LeetHub

--- 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
@@ -3,17 +3,23 @@ public:
     int minimumDeletions(string s) {
         
         int n = s.size();
-        int acount =0;
-        int ans =0;    //a should be on the left hand side of b
-        for(int i=n-1;i>=0;i--){
-           if(s[i]=='b' && acount>0){
-            acount--;
-            ans++;
-           }
-           else if(s[i]=='a'){
-            acount++;
-           }
+        stack<char>st;
+        int ans =0;
+
+        for(char ch:s){
+            if(ch == 'b'){
+                st.push(ch);
+            }
+            else if(ch == 'a'){
+                if(!st.empty() &&  st.top()=='b'){
+                    st.pop();
+                    ans++;
+                } 
+                else{ // stack is empty 
+                    st.push(ch);
+                }
+            }
         }
-  return ans;
+        return ans;
     }
 };
