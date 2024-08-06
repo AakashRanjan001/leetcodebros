c2e1ac2c8549da509305d5dde23e1f2d1352f77b
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-08-06 16:02:48 +0530

    Time: 71 ms (66.7%), Space: 24.8 MB (64.48%) - LeetHub

--- 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,33 +1,36 @@
 
 class Solution {
 public:
-    int minimumPushes(string word) {
-        unordered_map<char,int>mp;
-        for(auto ch:word){
-            mp[ch]++;  //for storing the frequency
+    int minimumPushes(std::string word) {
+        // Initialize frequency array for each letter
+        std::vector<int> letterCount(26, 0);
+
+        // Count the frequency of each letter in the word
+        for (char ch : word) {
+            letterCount[ch - 'a']++;
         }
-        vector<int>V;
-        for(auto c:mp){
-            V.push_back(c.second);
+
+        // Sort frequencies in descending order
+        std::sort(letterCount.begin(), letterCount.end(), std::greater<int>());
+
+        long long pushes = 0;
+        long long cnt = 0;
+
+        // Calculate minimum pushes needed
+        for (int i = 0; i < letterCount.size(); i++) {
+            cnt++;
+            if (letterCount[i] == 0) break;  // No need to count further if frequency is zero
+
+            if (cnt <= 8) {
+                pushes += 1 * letterCount[i];
+            } else if (cnt <= 16) {
+                pushes += 2 * letterCount[i];
+            } else if (cnt <= 24) {
+                pushes += 3 * letterCount[i];
+            } else {
+                pushes += 4 * letterCount[i];
+            }
         }
-        sort(V.begin(),V.end(),greater<int>());
-        int count=0;
-        int ans=0;
-        for(auto h:V){
-            count++;
-            if(count<=8){
-                ans+=1*h;
-            }
-            else if(count<=16){
-                ans+=2*h;
-            }
-            else if(count<=24){
-                ans+=3*h;
-            }
-            else{
-                ans+=4*h;
-            }
-        }
-        return ans;
+        return pushes;
     }
 };
