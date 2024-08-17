81eed5a0f7e84db358e6239d0a5a9150cecaaadc
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-08-17 16:05:25 +0530

    Time: 77 ms (7.17%), Space: 21.9 MB (43.64%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,42 +11,39 @@
  */
 class Solution {
 public:
-     int  height(TreeNode* root){
-          if(root == NULL){
-            return 0;
-          }
-
-         int leftheight = height(root->left);
-         int rightheight = height(root->right);
-
-         int ans =1 +  max(leftheight, rightheight);
-
-         return ans;
-     }
-
-
-   
-    bool isBalanced(TreeNode* root) {
-        //bas ecase 
+    int height(TreeNode* root){
         if(root == NULL){
-            return true;
+            return 0;
         }
-        //1 case solve karo do that is for root node 
         int leftheight = height(root->left);
         int rightheight = height(root->right);
-        int diff = abs(leftheight - rightheight);
 
-        bool ans1 = (diff<=1);
-
-        //recursiion sambhal lega
-        bool leftpart = isBalanced(root->left);
-        bool rightpart = isBalanced(root->right);
-
-        if(ans1 && leftpart && rightpart){
+        int maxi = max(leftheight, rightheight)+1;
+        return maxi;
+    }
+  
+    bool isBalanced(TreeNode* root) {
+        //balance dekhne ke liye check katrte hai whther the tree diff of the leftpart and the rightpart is less than equla to 1
+        //base case 
+         if(root == NULL){
             return true;
-        }
-        else{
-            return false;
-        }
+         }
+
+         // 1 case solve kar do
+         int leftheight = height(root->left);
+         int  rightheight= height(root->right);
+         int diff = abs(leftheight - rightheight);
+           bool ans1 = (diff<=1);
+
+         bool leftpart =isBalanced(root->left);
+         bool rightpart = isBalanced(root->right);
+
+      if(ans1 &&  leftpart && rightpart){
+        return true;
+      }
+      else{
+        return false;
+      }
+
     }
 };
