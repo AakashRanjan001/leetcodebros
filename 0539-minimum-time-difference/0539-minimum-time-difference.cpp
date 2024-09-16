db03c16eeedd915a03ab8a99bf69285c0f703a3f
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-09-16 10:34:59 +0530

    Time: 14 ms (41.48%), Space: 17.7 MB (60.05%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.cpp
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     int findMinDifference(vector<string>& timePoints) {
         
-        vector<int>minutes;
+        vector<int>minutes;   
         for(int i=0;i<timePoints.size();i++){
             string curr = timePoints[i];
             
-            int hours = stoi(curr.substr(0,2));
-            int minute = stoi(curr.substr(3,2));
+            int hours = stoi(curr.substr(0,2));  //first conversion of string part into int
+            int minute = stoi(curr.substr(3,2)); //means hours yaha string ke form mei hai toh apon ko int  mei badalte hai by use of stoi function
             int totalminutes = hours*60 + minute;
-            minutes.push_back(totalminutes);
+            minutes.push_back(totalminutes);  // fir jo resulatnt array hota in minutes ke form mei usko pushback kar dete
             
         }
-        sort(minutes.begin(),minutes.end());
-        
+        sort(minutes.begin(),minutes.end());  //apon ko min time diff chahiye so linear ek elemnt ko select karke dusre elemnt mei traverse karme mei ti,me compelxity is O^n2
+//But agar apon sort kar denge toh nlogn TC hoga which is less than 0n^2        
         int n = minutes.size();
-        int mini = INT_MAX;
+        int mini = INT_MAX; 
         
         for(int i=0;i<n-1;i++){
             int diff = abs(minutes[i+1]-minutes[i]);
             mini = min(mini,diff);
         }
         int lastdiff=(minutes[0]+1440)-minutes[n-1];//something missing
-        mini= min(mini ,lastdiff);
+        mini= min(mini ,lastdiff); // ye part hamesha bhul jaaat hu dhyan rakhna hoiga 
         
         return mini;
         
