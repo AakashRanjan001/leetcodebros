5664dca08c14ff959264b3843bef01f746262195
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-10-23 11:46:18 +0530

    Time: 11 ms (84.83%), Space: 17.7 MB (84%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -1,32 +1,36 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int cnt = 0;
+    int ans = 0; // to store the no.of paths from a single node 
+    void pathoneRoot(TreeNode* root , long long sum){
+        if(root == NULL)return;
 
-    void solve(TreeNode* root, int targetSum, long long csum) {
-        // base case
-        if (root == NULL) {
-            return;
+        if(sum == root->val){
+            ++ans;
         }
+       // preorder traversal karna hai 
+       pathoneRoot(root->left, sum - root->val);
+       pathoneRoot(root->right, sum - root->val);
 
-        csum += root->val;
-        if (csum == targetSum) {
-            cnt++;
-        }
-
-        solve(root->left, targetSum, csum);
-        solve(root->right, targetSum, csum);
     }
-
-    int pathSum(TreeNode* root, int targetSum) {
-        if (root == NULL) return 0; // Base case to terminate recursion
-
-        long long csum = 0;
-
-        solve(root, targetSum, csum);
-        pathSum(root->left, targetSum);
-        pathSum(root->right, targetSum);
-
-        return cnt;
+    int pathSum(TreeNode* root, long long targetSum) {
+        // hame har ek node ke corresponding paths check karne hai ki woh pathsum ke equAL HAI KI NHI 
+        if(root == NULL)return 0;
+        if(root!= NULL){
+            pathoneRoot(root , targetSum);
+            pathSum(root->left, targetSum);
+            pathSum(root->right,targetSum);
+        }
+        return ans;
     }
 };
