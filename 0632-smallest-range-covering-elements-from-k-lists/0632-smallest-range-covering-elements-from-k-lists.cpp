6afc46957f04131207a5dbded230a49b448b337a
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-12-07 19:00:58 +0530

    Time: 103 ms (35.32%), Space: 51.2 MB (5%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -1,53 +1,83 @@
+class node{
+    public:
+    int data;
+    int row;
+    int col;
+
+    node(int val, int r , int c){
+        data = val;
+        row = r;
+        col = c;
+    }
+
+};
+
+class compare{
+      public:
+      bool operator()(node* a , node* b){
+        return a->data > b->data;
+      }
+};
 
 class Solution {
 public:
     vector<int> smallestRange(vector<vector<int>>& nums) {
-        vector<pair<int, int>> arr;
-        int i = 1;
-        for (auto it : nums) {
-            for (auto e : it) {
-                arr.push_back({e, i});
-            }
-            i++;
-        }
-        sort(arr.begin(), arr.end());
-        int k = nums.size();
-        i = 0;
-        int j = 0;
-        vector<int> ans(2, 1e5);
-        ans[0] = -1e5;
-        unordered_map<int, int> mp;
-        //sliding window 
-        while (i < arr.size()) {
-            mp[arr[i].second]++;
-            if (mp.size() == k) {
-                while (mp[arr[j].second] > 1) {
-                    mp[arr[j].second]--;
-                    j++;
-                }
-                int diff = arr[i].first - arr[j].first;
+        int mini = INT_MAX;
+        int maxi = INT_MIN;
 
-                if (diff < ans[1] - ans[0]) {
-                    ans[0] = arr[j].first;
-                    ans[1] = arr[i].first;
-                }
-                mp.erase(arr[j].second);
-                j++;
-                while (j < i && mp[arr[j].second] > 1) {
-                    mp[arr[j].second]--;
-                    j++;
-                }
-            }
-            i++;
+        priority_queue<node* ,vector<node*>,compare>minheap;
+
+        // abb minheap mei first k elements ko push karna hai
+        int k = nums.size();
+       
+        for(int i=0;i<k;i++){
+           int element = nums[i][0];
+           mini = min(element, mini);
+           maxi = max(element,maxi);
+           minheap.push(new node(nums[i][0], i , 0 ));
+           
         }
+        int ansStart = mini;
+        int ansEnd = maxi;
+
+        while(!minheap.empty()){
+             node* top = minheap.top();
+             int topelement = top->data;
+             int toprow = top->row;
+             int topcol = top->col;
+
+             minheap.pop();
+
+             // now before further adding aother element we have to update mini
+             mini = topelement;// MINI UPDATED
+             // mini update hui hai matlab range update hui hai 
+             // check karna chahiye abhi jo mini update hui hai usse range kuch update hui hai 
+
+             int currRange = maxi-mini;
+             int ansRange = ansEnd - ansStart;
+             if(currRange < ansRange){
+                   ansStart = mini;
+                   ansEnd = maxi;
+             }
+
+        // abb jo value pop lari hai toh check karo kya uske aage koi value exist karti hai ki nhi 
+         if(topcol +1 < nums[toprow].size()){
+            // this was missed apon max ki value ko update nhi kiye the anfter insertion
+            maxi = max(nums[toprow][topcol+1],maxi);
+            minheap.push(new node(nums[toprow][topcol+1] , toprow , topcol+1));
+
+         }
+         else{
+            // maanlo aage element nhi hota 
+            break;
+         }
+      
+        }
+vector<int>ans;
+ans.push_back(ansStart);
+ans.push_back(ansEnd);
         return ans;
+        
+
     }
 };
-
-//optimization code
-auto init = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 'c';
-}();
