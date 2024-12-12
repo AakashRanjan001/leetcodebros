675b0919632bc18f91db1d445ef8836f9667a0c0
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-12-12 12:15:24 +0530

    Time: 0 ms (100%), Space: 13.4 MB (16.26%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
@@ -2,19 +2,31 @@ class Solution {
 public:
     long long pickGifts(vector<int>& gifts, int k) {
         
+        //jab bhi maximum ya minimum nikalna hai then we should always use priority queueu
+        priority_queue<int>pq;
         int n = gifts.size();
-        sort(gifts.rbegin(),gifts.rend());
-    
-        while(k--){
-            
-            gifts[0] = floor(sqrt(gifts[0]));
 
-            sort(gifts.rbegin(),gifts.rend());
-        }
-        long long sum =0;
         for(int i=0;i<n;i++){
-            sum+=gifts[i];
+            pq.push(gifts[i]);
         }
-        return sum;
+        while(k--){
+             int element = pq.top();
+             pq.pop();
+
+             element = floor(sqrt(element)); 
+             pq.push(element);
+        }  
+      vector<int>ans;
+      while(!pq.empty()){
+         ans.push_back(pq.top());
+         pq.pop();
+      }
+      long long sum =0;
+      for(int i=0;i<ans.size();i++){
+        sum+=ans[i];
+      }
+      return sum;
+     
+
     }
 };
