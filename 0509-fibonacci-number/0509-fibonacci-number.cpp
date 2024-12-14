126ed558d5fe03ff58c05f0c6296e8c0d230c4d3
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-12-14 12:47:36 +0530

    Time: 7 ms (52.25%), Space: 7.9 MB (10.87%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    int fib(int n) {
-        
-        if(n==0){
-            return 0;
+   int getRecSolve(int n){
+       // base case
+        if(n == 0 || n==1){
+             return n;
         }
-        if(n==1){
-            return 1;
-        }
-
-        int ans = fib(n-1)+fib(n-2);
+        int ans = getRecSolve(n-1) + getRecSolve(n-2);
         return ans;
+     
+   }
+    int fib(int n) {
+         int ans = getRecSolve(n);
+         return ans;
     }
 };
