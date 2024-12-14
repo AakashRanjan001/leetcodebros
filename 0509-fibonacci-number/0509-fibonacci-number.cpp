a6abb09113d36cde91953c5680ff30850215b254
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-12-14 14:56:17 +0530

    Time: 0 ms (100%), Space: 7.9 MB (10.87%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -23,27 +23,48 @@ public:
 //         dp[n] = topDownSolve(n-1,dp) + topDownSolve(n-2,dp);
 //         return dp[n];
 //    }
-    int bottomUpSolve(int n){
-         // step 1:
-          // creat ea DP array
-          vector<int>dp(n+1 , -1);
+    // int bottomUpSolve(int n){
+    //      // step 1:
+    //       // creat ea DP array
+    //       vector<int>dp(n+1 , -1);
 
-          // step 2:base casejo tumne top down mei likha tha usko upgrade karo
-          dp[0] = 0;
-          dp[1] = 1;
+    //       // step 2:base casejo tumne top down mei likha tha usko upgrade karo
+    //       dp[0] = 0;
+    //       if(n==0)return dp[0];
+    //       dp[1] = 1;
+    //       if(n==1)return dp[1];
 
-          //step 3: 
-          // perform the iterative approach // ulta as chalenge of top down
-          for(int i=2;i<=n;i++){
-             dp[i] = dp[i-1] + dp[i-2];
-          }
-          return dp[n];
+    //       //step 3: 
+    //       // perform the iterative approach // ulta as chalenge of top down
+    //       for(int i=2;i<=n;i++){
+    //          dp[i] = dp[i-1] + dp[i-2];
+    //       }
+    //       return dp[n];
 
+    // }
+    int spaceOptSolve(int n){
+         
+         //step2: base case
+         int prev2 =0;
+         int prev1 =1 ;
+
+// yaha mei galti hui
+      if(n==0)return prev2;
+      if(n==1)return prev1;
+
+         int curr;
+         for(int i=2;i<=n;i++){
+             curr = prev2 + prev1;
+             prev2 = prev1;
+             prev1 = curr;
+         }
+         return curr;
     }
 
    
     int fib(int n) {
-     return bottomUpSolve(n);
+    //  return bottomUpSolve(n);
+    return spaceOptSolve(n);
      
 
     }
