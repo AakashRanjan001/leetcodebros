a40555734d53e4455a08060fff814872b7268437
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-12-15 19:16:35 +0530

    Time: 0 ms (100%), Space: 10.2 MB (23.89%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -69,6 +69,26 @@ int  solveUsingTab(vector<int>& nums , int n){
         return dp[n];
 }
 
+int spaceOptimisedSol(vector<int>& nums,int n){
+    int prev2=0;
+    int prev1 = nums[0];
+    int curr =0;
+
+    for(int i=1;i<=n;i++){
+          int temp = 0;
+            if(i-2>=0)
+                temp = prev2;
+              int include = temp + nums[i];  
+                int exclude = prev1 + 0; 
+
+                curr = max(include,exclude);
+                prev2 = prev1;
+                prev1 = curr;
+
+    }
+    return prev1;
+}
+
     int rob(vector<int>& nums) {
         int n = nums.size()-1; // index of the last house 
         // int ans = solveUsingRec(nums,n);
@@ -85,8 +105,10 @@ int  solveUsingTab(vector<int>& nums , int n){
 
 
 
-         int ans = solveUsingTab(nums,n);
-         return ans;
+        //  int ans = solveUsingTab(nums,n);
+        //  return ans;
 
+
+        return spaceOptimisedSol(nums,n);
     }
 };
