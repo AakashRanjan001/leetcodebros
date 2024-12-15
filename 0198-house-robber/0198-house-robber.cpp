62e2a7cf6f83aba5c52baf5bf8e9ee4fb272b94b
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-12-15 18:49:40 +0530

    Time: 0 ms (100%), Space: 10.8 MB (7.63%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -20,39 +20,73 @@ public:
 
 //          return max(include,exclude);
     
+// //     }
+
+//     int solveUsingMem(vector<int>& nums , int n , vector<int>& dp){
+//          if(n<0)return 0; 
+//         if(n == 0){ 
+//              return nums[0];
+//         }
+
+// // step 3 : check if the dp[n] already exist or not 
+//         if(dp[n]!=INT_MIN){
+//              return dp[n];
+//         }
+        
+//         // include the house 
+//         // calling the house which 2 steps behind and also including the money of current house
+//          int include = solveUsingMem(nums,n-2,dp) + nums[n]; 
+//         // exclude teh house 
+//         // means not taking that house means i can take the next adjacent house 
+//          int exclude = solveUsingMem(nums,n-1,dp) + 0; 
+
+//        //step 2: dp[n] include kar do 
+//          dp[n] = max(include,exclude);
+//          return dp[n];
+
 //     }
 
-    int solveUsingMem(vector<int>& nums , int n , vector<int>& dp){
-         if(n<0)return 0; 
-        if(n == 0){ 
-             return nums[0];
-        }
+int  solveUsingTab(vector<int>& nums , int n){
+     //step 1 : create an array 
+     vector<int>dp(n+1 , 0);
 
-        if(dp[n]!=INT_MIN){
-             return dp[n];
-        }
+     //step 2:base case ko dekho 
+     dp[0] = nums[0];
+
+        // in top down moves from n->2 
+        ///step 3: 
+        for(int i=1;i<=n;i++){
         
-        // include the house 
-        // calling the house which 2 steps behind and also including the money of current house
-         int include = solveUsingMem(nums,n-2,dp) + nums[n]; 
-        // exclude teh house 
-        // means not taking that house means i can take the next adjacent house 
-         int exclude = solveUsingMem(nums,n-1,dp) + 0; 
+        int temp = 0;
+            if(i-2>=0)
+                temp = dp[i-2];
+              int include = temp + nums[i];  
+                int exclude = dp[i-1] + 0; 
 
-         dp[n] = max(include,exclude);
-         return dp[n];
+                dp[i] = max(include,exclude);
+            
+        }
+        return dp[n];
+}
 
-    }
     int rob(vector<int>& nums) {
         int n = nums.size()-1; // index of the last house 
         // int ans = solveUsingRec(nums,n);
         // return ans;
 
         // step 1 : Creating an array 
-        vector<int>dp(n+1,INT_MIN);
-        int ans = solveUsingMem(nums,n,dp);
-        return ans;
+        // agar size samajh mei nhi aa rha kya rakhna hai tab dekhna ki variable kya hai n yah avary kar rha 
+        // as hame array mei store kya kara rhe har ek house ke corresponding maximum money
+        // isi kaaran INT_MIN kiya 
+        // vector<int>dp(n+1,INT_MIN);
+        // int ans = solveUsingMem(nums,n,dp);
+        // return ans;
 
+
+
+
+         int ans = solveUsingTab(nums,n);
+         return ans;
 
     }
 };
