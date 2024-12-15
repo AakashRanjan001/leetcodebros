8ece3b91d17828cf527dea32ffe19a6032486656
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-12-15 18:20:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,13 +1,27 @@
-
 class Solution {
-public: 
-    int rob(vector<int>& nums) {
-        int n = nums.size(), pre = 0, cur = 0;
-        for (int i = 0; i < n; i++) {
-            int temp = max(pre + nums[i], cur);
-            pre = cur;
-            cur = temp;
+public:
+  // n -> is the index of teh current house 
+    int solveUsingRec(vector<int>& nums , int n){
+        if(n<0)return 0; // agar ek bhi ghar nhi hai toh money = 0
+        if(n == 0){
+             // agar ek hi ghar hai 
+             return nums[0];
         }
-        return cur;
+        
+        // include the house 
+        // calling the house which 2 steps behind and also including the money of current house
+         int include = solveUsingRec(nums,n-2) + nums[n]; 
+        // exclude teh house 
+        // means not taking that house means i can take the next adjacent house 
+         int exclude = solveUsingRec(nums,n-1) + 0; 
+
+         return max(include,exclude);
+
+
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size()-1; // index of the last house 
+        int ans = solveUsingRec(nums,n);
+        return ans;
     }
 };
