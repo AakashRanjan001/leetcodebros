47dea923baa35755a0a9c252ee04c8ccfa9a1bbe
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-12-19 13:03:16 +0530

    Time: 47 ms (13.39%), Space: 27.5 MB (18.56%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,27 +1,56 @@
-
 class Solution {
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-        if (text1.length() < text2.length()) {
-            return LCS(text1, text2);
-        }
-        return LCS(text2, text1);       //answer not solved
+    int solveUsingRec(string a , string b , int i , int j){
+         //base case 
+         if(i == a.length())return 0;
+         if(j == b.length())return 0;// jab donoo end of the string aa gye tab rukna hai
+
+         //if char match 
+         int ans =0;
+         if(a[i] == b[j]){
+             ans = 1 + solveUsingRec(a,b,i+1,j+1);
+             // match hu toh curr char ko toh include karenge phir baaki recursion kaerga 
+         }
+         else{
+             ans =  0 + max(solveUsingRec(a,b,i,j+1) , solveUsingRec(a,b,i+1,j));
+         }
+         return ans;
     }
+    int solveUsingMem(string &a , string &b , int i , int j,  vector<vector<int>>&dp){
+         //base case 
+         if(i == a.length())return 0;
+         if(j == b.length())return 0;// jab donoo end of the string aa gye tab rukna hai
 
-    int LCS(string s1, string s2) {
-        vector<vector<int>> M(2, vector<int>(s1.length() + 1, 0));
+ //step 3: check if answer already exists
+ if(dp[i][j]!=-1){
+     return dp[i][j];
+ }
 
-        for (int i = 1; i <= s2.length(); i++) {
-            M[i % 2][0] = 0;
-            for (int j = 1; j <= s1.length(); j++) {
-                if (s1[j - 1] == s2[i - 1]) {
-                    M[i % 2][j] = M[(i - 1) % 2][j - 1] + 1;
-                } else {
-                    M[i % 2][j] = max(M[(i - 1) % 2][j - 1],
-                                max(M[(i - 1) % 2][j], M[i % 2][j - 1]));
-                }
-            }
-        }
-        return M[s2.length() % 2][s1.length()];
+
+//step 2: answwre mei dp array daalo 
+         //if char match 
+         int ans =0;
+         if(a[i] == b[j]){
+             dp[i][j] = 1 + solveUsingMem(a,b,i+1,j+1,dp);
+             // match hu toh curr char ko toh include karenge phir baaki recursion kaerga 
+         }
+         else{
+             dp[i][j] =  0 + max(solveUsingMem(a,b,i,j+1,dp) , solveUsingMem(a,b,i+1,j,dp));
+         }
+         return dp[i][j];
+    }
+    
+    int longestCommonSubsequence(string text1, string text2) {
+        int i=0;
+        int j=0;
+        // int ans = solveUsingRec(text1 , text2 , i, j);
+        // return ans;
+
+       // MEMOIZATION PROBLEM 
+       //step1 : creata a dp array 
+       vector<vector<int>>dp(text1.length(),vector<int>(text2.length() , -1));
+        int ans = solveUsingMem(text1 , text2,i,j , dp);
+        return ans;
+        
     }
 };
