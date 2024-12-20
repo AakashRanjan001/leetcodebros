b7e21e1d610cbfe92fd63d1caecb236eac278303
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-12-20 15:26:40 +0530

    Time: 390 ms (23.59%), Space: 576.2 MB (5.89%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -21,11 +21,14 @@ public:
          int ans = max(include, exclude);
          return ans;
     }
-    int solveUsingMem(vector<int>& nums, int curr, int prev , vector<vector<int>>&dp){
+     int solveUsingMem(vector<int>& nums, int curr, int prev , vector<vector<int>>&dp){
          if(curr>=nums.size()){
             return 0;
          }
          int new_prev = prev +1 ;
+         // we did this because in prev  = -1 it is not a valid index and hence 
+         // we have to add +1 for this to make it valid in the dp array
+
          // step 3: just after th base case check if the value already exisd
          if(dp[curr][new_prev ]!=-1){
              return dp[curr][new_prev ];
@@ -47,6 +50,33 @@ public:
          return dp[curr][new_prev ];
     }
 
+    int solveUsingTab(vector<int>& nums){
+        int n = nums.size();
+         //step 1: create a dp array 
+          vector<vector<int>>dp(n+1, vector<int>(n+1 , 0));
+
+          // step 2: base case dekho , no need alll are initialised to 0
+
+          // step 3:iterative approach two for loop s
+          for(int curr = n-1;curr>=0;curr--){
+            for(int prev = curr-1;prev>=-1 ; prev--){
+              int include =0;
+              if(prev == -1 || nums[curr]>nums[prev]){
+              include = 1 + dp[curr+1][curr +1];// we have done curr as for validindex
+            
+             }
+  
+         // excude 
+            int exclude = 0 + dp[curr+1][prev +1];
+            int ans = max(include, exclude);
+             dp[curr][prev +1] =ans;
+   
+            }
+          }
+        return dp[0][-1+1];
+    }
+   
+
     int lengthOfLIS(vector<int>& nums) {
         
         int n = nums.size();
@@ -58,7 +88,13 @@ public:
            // columns = -1--> n-1 => n+1 cols
        // step 1 : create a dp array 
        vector<vector<int>>dp(n, vector<int>(n+1 , -1));
-        int ans = solveUsingMem(nums,curr, prev , dp);
-        return ans;
+        // int ans = solveUsingMem(nums,curr, prev , dp);
+        // return ans;
+
+      
+
+      // TABULATION 
+      int ans = solveUsingTab(nums);
+      return ans;
     }
 };
