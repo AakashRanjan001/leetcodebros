83c04de23e73c71f3a29f987f00d7a229c105eb9
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-12-21 13:36:22 +0530

    Time: 74 ms (48.05%), Space: 13.9 MB (23.22%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,37 +1,64 @@
 class Solution {
 public:
-    int numSquares(int n) {
-        
-vector<int>ans;
-int x = 1;
-while(x*x<=n){
-    ans.push_back(x*x);
-    x++;
-}
-
-for(int i=0;i<ans.size();i++){
-    if(ans[i]==n){         //agar mera n hi perfect sqaure hua toh ek hi baar mei ho jayega
-        return 1;
+    int numSQHelper(int n){
+          // base case 
+        if(n==0)return 1; //means jab n== 0 hua tab sum ban chuka hai that is 
+        // equal to n 
+        int i=1;
+        int ans = INT_MAX;
+        int end = sqrt(n);
+        while(i<=end){
+        int perfectSquare = i*i;
+          int numberOfPerfectSQ  = 1 + numSQHelper(n-perfectSquare);
+          ans = min(ans , numberOfPerfectSQ);
+          i++;
+        }
+        // but yaha ek aur extra function call aa rha hai thats why at the end we did a -1
+        return ans;
     }
-}
-  for (int i = 0; i < ans.size(); i++) {
-            for (int j = i; j < ans.size(); j++) {
-                if (ans[i] + ans[j] == n) {
-                    return 2;
-                }
-            }
-        }
+    int numSQHelperMem(int n ,vector<int>&dp ){
+      if(n==0)return 1; //means jab n== 0 hua tab sum ban chuka hai that is 
+        // equal to n 
 
-            for (int i = 0; i < ans.size(); i++) {
-            for (int j = i; j < ans.size(); j++) {
-                for (int k = j; k < ans.size(); k++) {
-                    if (ans[i] + ans[j] + ans[k] == n) {
-                        return 3;
-                    }
-                }
-            }
+      //step 3: check if the answer already existd
+      if(dp[n]!=-1){
+        return dp[n];
+      }
+
+        // step 2: ans wale mei dp ka daalo 
+        int i=1;
+        int ans = INT_MAX;
+        int end = sqrt(n);
+        while(i<=end){
+        int perfectSquare = i*i;
+          int numberOfPerfectSQ  = 1 + numSQHelperMem(n-perfectSquare ,dp);
+          ans = min(ans , numberOfPerfectSQ);
+          i++;
         }
-        
-        return 4;
+        dp[n] = ans;
+        // but yaha ek aur extra function call aa rha hai thats why at the end we did a -1
+        return dp[n];
+    }
+    int numSquares(int n) {
+        // INitial thought 
+        // // base case 
+        // if(n==0)return 1; //means jab n== 0 hua tab sum ban chuka hai that is 
+        // // equal to n 
+        // int i=1;
+        // int perfectSquare = i*i;
+
+        // int ans = 1 + numSquares(n-perfectSquare);
+        // // but yaha ek aur extra function call aa rha hai 
+        // return ans;
+
+        // int ans = numSQHelper(n) -1;
+        // return ans;
+
+        //1d dp 
+        // step1 : create a  dp array 
+        vector<int>dp(n+1 , -1);
+        int ans = numSQHelperMem(n , dp) -1;
+        return ans;
+
     }
 };
