10bf2859b1b5d61e590e879a2c58610c9916cf81
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-12-25 13:12:02 +0530

    Time: 0 ms (100%), Space: 10.2 MB (23.69%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -13,9 +13,39 @@ public:
         int ans = max(diffbystart,diffbyend);
         return ans;
      }
+
+     int solveUsingMem(vector<int>& nums,int start, int end,  vector<vector<int>>&dp){
+        int n = nums.size();
+         if(start == end)return nums[start];
+        
+
+        if(dp[start][end]!=-1){
+             return dp[start][end];
+        }
+        //diff-> p1 - p2
+        //p1 --> nums[start] pick kiya , bakki recursion p2-->ka laayega 
+        int diffbystart = nums[start] -solveUsingMem(nums, start+1,end,dp);
+        int diffbyend = nums[end] - solveUsingMem(nums,start,end -1,dp);
+
+        //agar meko player 1 ko jeetnaa hai toh meko diiff maximise karna hoga 
+        int ans = max(diffbystart,diffbyend);
+        dp[start][end] = ans;
+        return dp[start][end];
+        
+
+     }
     bool predictTheWinner(vector<int>& nums) {
         //start = 0 , end = nums.size()-1
-        int ans = solveUsingRec(nums,0,nums.size()-1);
+        // int ans = solveUsingRec(nums,0,nums.size()-1);
+        // return ans>=0;
+
+         int n = nums.size();
+        //memoiaxztion
+        //start index --> 0 -> end;//end can be till n
+        //end index ---> num.size()-1->start // start can be till 0 index
+        //step 1: Create a dp array 2d dp
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        int ans = solveUsingMem(nums,0,n-1,dp);
         return ans>=0;
 
     }
