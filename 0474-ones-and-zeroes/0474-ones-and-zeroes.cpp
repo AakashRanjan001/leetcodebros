d37d17a10e32da92356dcbc31fd62625cdc2a57d
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-12-29 16:30:40 +0530

    Time: 215 ms (22.32%), Space: 106.7 MB (13.47%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -44,8 +44,33 @@ public:
              include = 1 + solveUsingMem(NumStrs,i+1,m-zeroes,n-ones,dp);
          }
          exclude = 0 + solveUsingMem(NumStrs,i+1,m,n,dp);
-         dp[i][m][n] = max(include,exclude);
+        
          return dp[i][m][n];
+    }
+    int solveUsingTab(vector<pair<int,int>>&NumStrs, int m, int n){
+        //step 1: create a Dp array 
+        vector<vector<vector<int>>>dp(NumStrs.size()+1,vector<vector<int>>(m+1 ,vector<int>(n+1,0)));
+        //step 2: base case dekho but all are 0 hence not required 
+
+        //step 3: iterative approach 3 for loops
+        for(int i=NumStrs.size()-1;i>=0;i--){
+            for(int j =0;j<=m;j++){
+                for(int k=0;k<=n;k++){
+               
+
+                 int zeroes = NumStrs[i].first;
+                 int ones = NumStrs[i].second;
+                 int include =0,exclude =0;
+                  //include/exclude pattern
+                 if(j - zeroes>=0 && k - ones>=0){
+                     include = 1 + dp[i+1][j-zeroes][k-ones];
+                  }
+                   exclude = 0 + dp[i+1][j][k];
+                   dp[i][j][k] = max(include,exclude);
+                }
+            }            
+        }    
+         return dp[0][m][n];
     }
 
     int findMaxForm(vector<string>& strs, int m, int n) {
@@ -60,8 +85,13 @@ public:
        // we have to use 3D DP
        //step 1: create a 3D Dp  'i' varies from 0 -->NumStrs.size() --> size +1
        // m ---> varies from m--->0 => m+1 
-         vector<vector<vector<int>>>dp(NumStrs.size()+1,vector<vector<int>>(m+1 ,vector<int>(n+1,-1)));
-        int ans = solveUsingMem(NumStrs,0,m,n,dp);
+        //  vector<vector<vector<int>>>dp(NumStrs.size()+1,vector<vector<int>>(m+1 ,vector<int>(n+1,-1)));
+        // int ans = solveUsingMem(NumStrs,0,m,n,dp);
+        // return ans;
+
+
+        //tabulatiom 
+        int ans = solveUsingTab(NumStrs,m,n);
         return ans;
     }
 };
