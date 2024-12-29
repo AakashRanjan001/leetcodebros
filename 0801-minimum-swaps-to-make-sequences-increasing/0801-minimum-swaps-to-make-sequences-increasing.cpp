8b81a2e502c2e38f9c7bb3412d5e1f5200d7ae43
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-12-29 19:30:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -19,6 +19,13 @@ public:
     }
     int solveUsingMem(vector<int>& nums1, vector<int>& nums2,int i,int p1,int p2,vector<vector<int>> &dp,int isSwap ){
             if(i==nums1.size())return 0;
+         
+         //step 3: check if the answr laready exists 
+          if(dp[i][isSwap]!=-1){
+             return dp[i][isSwap];
+          }
+
+         // step2 
          int swap = INT_MAX,not_swap = INT_MAX;
           ////swap condition 
           if(p1<nums2[i] && p2<nums1[i]){ // 1 represents swap done 
@@ -32,6 +39,34 @@ public:
           dp[i][isSwap] = min(swap,not_swap);
           return dp[i][isSwap];
     }
+    int solveUsingTab(vector<int>& nums1, vector<int>& nums2){
+         //step 1: create the dp array 
+          vector<vector<int>>dp(nums1.size()+1,vector<int>(2,0));
+          //step 2: check the base case please write some answerds prerequisite 
+        //   no need
+
+        //step 3: for loops  // look in the notebook // j== 1->swap and j=0->non_swap case 
+          for(int i= nums1.size()-1;i>=1;i--){
+             for(int j= 1;j>=0;--j){
+                   int p1 = nums1[i-1];
+                   int p2 = nums2[i-1];
+                         int swap = INT_MAX,not_swap = INT_MAX;
+          ////swap condition 
+                if(p1<nums2[i] && p2<nums1[i]){ // 1 represents swap done 
+                  swap = 1 + dp[i+1][1];
+                }
+
+          //not swap cond
+                 if(p1<nums1[i] && p2<nums2[i]){ // 0 reprsesnts swap not done 
+                   not_swap = dp[i+1][0];
+                 }
+                dp[i][j] = min(swap,not_swap);
+             
+
+             }
+          }
+              return dp[1][0];
+    }
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
         
         // int ans = solveUsingRec(nums1,nums2,0, -1,-1);
@@ -40,10 +75,20 @@ public:
         
         // i is changing from => 0----> nums.size() => n+1 ;
         // taking an isSwap condition that is swap/notswap , that is 2
-        vector<vector<int>>dp(nums1.size()+1,vector<int>(2,-1));
+        //step 1
+        // vector<vector<int>>dp(nums1.size()+1,vector<int>(2,-1));
         //isSwap = 0 currently no swap is taken right now 
-        int ans = solveUsingMem(nums1,nums2,0,-1,-1,dp,0 );
-        return ans;
+        // int ans = solveUsingMem(nums1,nums2,0,-1,-1,dp,0 );
+        // return ans;
+
+ 
+
+ // TABULATION
+     nums1.insert(nums1.begin(),-1);
+     nums2.insert(nums2.begin(),-1);
+     int ans = solveUsingTab(nums1,nums2);
+     return ans;
+
 
     }
 };
