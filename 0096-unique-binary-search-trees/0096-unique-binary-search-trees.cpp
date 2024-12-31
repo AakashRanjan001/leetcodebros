1e3c3aa0d966e6b889080390abc91cc82d1388eb
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-12-31 13:19:08 +0530

    Time: 0 ms (100%), Space: 8 MB (18.7%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -8,7 +8,27 @@ public:
      }
       return ans;
    }
+   int solveUsingMem(int n,vector<int>&dp ){
+     // base case
+     if(n<=1)return 1;
+
+     if(dp[n]!=-1)return dp[n];
+
+     int ans = 0;// considering every node as root node 
+     for(int i=1;i<=n;i++){
+        ans+= solveUsingMem(i-1,dp)*solveUsingMem(n-i,dp);
+     }
+      dp[n] = ans;
+      return dp[n];
+      
+   }
+
     int numTrees(int n) {
-       return solve(n); 
+    //    return solve(n);
+
+    vector<int>dp(n+1,-1);
+    int ans = solveUsingMem(n,dp);
+    return ans;
+
     }
 };
