8b758f283402ab484c9c94f00a227ecd054f9be1
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2025-01-02 14:01:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -26,14 +26,39 @@ public:
           return dp[i][j] =  ans;
 
     }
+
+    int solveUsingTab(string s, string t){
+        //step 1:creation of a dp array 
+         vector<vector<int>>dp(s.size()+2 ,vector<int>(t.size()+2,0));
+         //step 2: base case dekho 
+         for(int i=0;i<=s.size();i++){
+             dp[i][t.size()] =1;
+         }
+
+
+         for(int i=s.size()-1;i>=0;i--){
+             for(int j=t.size()-1;j>=0;j--){
+             int ans =0;
+             if(s[i] == t[j]){
+             ans+=dp[i+1][j+1];
+             }
+             ans+=dp[i+1][j];
+              dp[i][j] =  ans;
+             }
+         }
+        return dp[0][0] ;
+    }
     int numDistinct(string s, string t) {
         // int ans = solveUsingRec(s,t,0,0);
         // return ans;
 
         //here both i and j are changing hence we use 2D DP
         //step 1: creation of a DP array
-        vector<vector<int>>dp(s.size()+1 ,vector<int>(t.size()+1,-1));
-        int ans = solveUsingMem(s,t,0,0,dp);
+        // vector<vector<int>>dp(s.size()+1 ,vector<int>(t.size()+1,-1));
+        // int ans = solveUsingMem(s,t,0,0,dp);
+        // return ans;
+
+        int ans = solveUsingTab(s,t);
         return ans;
     }
 };
