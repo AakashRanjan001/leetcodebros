f019b343db00bc426787c9c41ed3b055ae5363ab
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2025-01-03 17:20:08 +0530

    Time: 92 ms (8.63%), Space: 35.5 MB (5.03%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    void bfs(map<pair<int,int>,bool> &visited, int row, int col ,vector<vector<char>>& grid ){
+           queue<pair<int,int>>q;//step1 
+           // perform the steps 
+           q.push({row,col});
+           visited[{row,col}] = true;
+    
+
+           while(!q.empty()){
+              pair<int,int>fnode = q.front();
+              q.pop();
+              int x = fnode.first;
+              int y = fnode.second;
+
+              // now after defining the coordinates i can move in every directions four ways
+              int dx[] = {-1,0,1,0};
+              int dy[] ={0,1,0,-1};
+
+              for(int i=0;i<4;i++){
+                  int newX = x + dx[i];
+                  int newY = y + dy[i];
+
+                  if(newX>=0 && newX<grid.size() && newY>=0 && newY<grid[0].size() && 
+                       !visited[{newX,newY}] && grid[newX][newY] == '1'){
+                          q.push({newX,newY});
+                          visited[{newX,newY}] = true;
+
+                       }
+              }
+           }
+
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        // hame bfs karna hai na 
+        map<pair<int,int>,bool>visited;
+      
+
+        int cnt =0;
+        for(int row =0;row<grid.size();row++){
+            int n = grid[row].size();
+             for(int col = 0;col<n;col++){
+                 if(!visited[{row,col}] && grid[row][col] == '1'){
+                     bfs(visited,row,col,grid);
+                     cnt++;
+                 }
+             }
+        }
+        return cnt;
+    }
+};
