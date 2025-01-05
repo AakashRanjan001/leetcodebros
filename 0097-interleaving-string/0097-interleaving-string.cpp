7c1a7cea66f84d3747fbf2dda56f495bbdc22054
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2025-01-05 16:46:07 +0530

    Time: 27 ms (13.97%), Space: 83.3 MB (5.59%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool solveUsingRec(string &s1,string&s2 , string &s3,int i,int j,int k){
+         //base case
+         if(i==s1.size() && j == s2.size() && k == s3.size())return true;
+
+          bool flag = false;
+         if(i<s1.size() && s1[i] == s3[k]){
+            flag=flag || solveUsingRec(s1,s2,s3,i+1,j,k+1);
+            // we have done OR becuase flag ka value change tab hota hai jab apon return karte hai 
+             // agar s1 == s3 hi ho gya matlab s2 jaane ki jaroorat hi nhi hai falg = false || true ho jayega and hence always true bann jayega 
+         }
+         if( j< s2.size() && s2[j] == s3[k]){
+            flag=flag || solveUsingRec(s1,s2,s3,i,j+1,k+1);
+         }
+         return flag;
+    }
+bool solveUsingMem(string &s1,string&s2 , string &s3,int i,int j,int k,   vector<vector<vector<int>>> &dp){
+     if(i==s1.size() && j == s2.size() && k == s3.size())return true;
+
+      if(dp[i][j][k]!=-1)return dp[i][j][k];
+
+          bool flag = false;
+         if(i<s1.size() && s1[i] == s3[k]){
+            flag=flag || solveUsingMem(s1,s2,s3,i+1,j,k+1,dp);
+            // we have done OR becuase flag ka value change tab hota hai jab apon return karte hai 
+             // agar s1 == s3 hi ho gya matlab s2 jaane ki jaroorat hi nhi hai falg = false || true ho jayega and hence always true bann jayega 
+         }
+         if( j< s2.size() && s2[j] == s3[k]){
+            flag=flag || solveUsingMem(s1,s2,s3,i,j+1,k+1,dp);
+         }
+         return dp[i][j][k] = flag;
+}
+     
+    bool isInterleave(string s1, string s2, string s3) {
+        // bool ans = solveUsingRec(s1,s2,s3,0,0,0);
+        // return ans;
+
+      
+      vector<vector<vector<int>>> dp(s1.size() + 1, vector<vector<int>>(s2.size() + 1, vector<int>(s3.size() + 1, -1)));
+
+        bool ans = solveUsingMem(s1,s2,s3,0,0,0,dp);
+        return ans;
+    }
+
+};
