4017d3df60144444051f159500c92f619258c074
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2025-01-07 12:41:45 +0530

    Time: 0 ms (100%), Space: 11.9 MB (27.31%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.cpp
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.cpp
@@ -1,12 +1,21 @@
+ class compare{
+     public:
+       bool operator()(string &a,string &b){
+         return a.size()<b.size();
+       }
+ };
 class Solution {
 public:
+
     vector<string> stringMatching(vector<string>& words) {
         int n = words.size();
       
       vector<string>ans;
-        sort(words.begin(),words.end(),[](string a,string b){
-            return a.size()<b.size();
-        });
+        // sort(words.begin(),words.end(),[](string a,string b){
+        //     return a.size()<b.size(); // sorted according to the length 
+        // });
+
+        sort(words.begin(),words.end(),compare());
       for(int i=0;i<n;i++){
          for(int j=i+1;j<n;j++){
              if( i!=j && words[j].find(words[i])!=string::npos){
