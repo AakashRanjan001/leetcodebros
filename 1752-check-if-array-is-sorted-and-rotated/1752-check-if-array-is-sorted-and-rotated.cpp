cf00eca569f1e31fad58ae3892b50f29e02cb8b2
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2025-02-02 12:32:47 +0530

    Time: 3 ms (2.2%), Space: 13.2 MB (5.21%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        //binary searh ka question lag rha hai 
+        int n = nums.size();
+        vector<int>temp = nums;
+
+        sort(temp.begin(),temp.end());
+
+        vector<int>ans(2*n);
+        for(int i=0;i<n;i++){
+            ans[i] = nums[i];
+        }
+        for(int i=0;i<n;i++){
+            ans[n+i] = nums[i];
+        }
+        // for(int i=0;i<ans.size();i++){
+        //     cout<<ans[i]<<"";
+        // }
+
+        for (int i = 0; i < n; i++) {
+            if (vector<int>(ans.begin() + i, ans.begin() + i + n) == temp) {
+                return true;
+            }
+        }
+     return false;
+    }
+};
