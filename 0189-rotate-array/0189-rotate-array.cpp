667486309fd4eed68d12943173cf5b986d794a81
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2025-02-03 00:45:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,20 +2,24 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         
-         int n = nums.size();
-         k = k%n;
-         vector<int>concatenate(2*n);
-         for(int i=0;i<n;i++){
-             concatenate[i] = nums[i];
-             concatenate[i+n] = nums[i];
-         }
-        //  for(int i=0;i<2*n;i++){
-        // cout<<concatenate[i]<<"";
-        //  }
+    //      int n = nums.size();
+    //      k = k%n;
+    //      vector<int>concatenate(2*n);
+    //      for(int i=0;i<n;i++){
+    //          concatenate[i] = nums[i];
+    //          concatenate[i+n] = nums[i];
+    //      }
+    //     //  for(int i=0;i<2*n;i++){
+    //     // cout<<concatenate[i]<<"";
+    //     //  }
 
-       for(int i=0;i<n;i++){
-           nums[i] = concatenate[n-k +i];
-       }
+    //    for(int i=0;i<n;i++){
+    //        nums[i] = concatenate[n-k +i];
+    //    }
+
+    reverse(nums.begin(),nums.end());
+    reverse(nums.begin(),nums.begin()+k);
+    reverse(nums.begin()+k,nums.end());
         
     }
 };
