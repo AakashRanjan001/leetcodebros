892200ce9f97a4c0e0187cb95a1629973592c1ce
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2025-02-03 17:24:57 +0530

    Time: 355 ms (5.06%), Space: 21.6 MB (92.49%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+
+
+        //BRUTE force
+        int n = nums.size();
+        bool flag =0;
+        for(int i=1;i<=n;i++){
+            flag =0;
+            for(int j=0;j<n;j++){
+                 if(nums[j] == i){
+                     flag = 1;
+                     break;
+                 }
+            }
+            if(flag == 0)return i;
+        }
+        return 0;
+
+
         //sum wala tareeka 
         // int sum =0;
         // int sum2 =0;
@@ -13,14 +31,14 @@ public:
         // return sum - sum2;
 
         //XOR wla tareeka 
-        int XOR =0;
-        int XOR1 =0;
-        for(int i=0;i<=nums.size();i++){
-             XOR^=i;
-        }
-        for(int i=0;i<nums.size();i++){
-             XOR1^=nums[i];
-        }
-        return XOR ^ XOR1;
+        // int XOR =0;
+        // int XOR1 =0;
+        // for(int i=0;i<=nums.size();i++){
+        //      XOR^=i;
+        // }
+        // for(int i=0;i<nums.size();i++){
+        //      XOR1^=nums[i];
+        // }
+        // return XOR ^ XOR1;
     }
 };
