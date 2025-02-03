af247f018dfec8e4ca11b563d4d9b381643b0fbc
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2025-02-03 17:42:02 +0530

    Time: 7 ms (22.26%), Space: 23.8 MB (14.07%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+
+     // 3 ways 
+    //   1. unordered_map wala tareeka
+    //   2. sort karke kar lenge
+    //   3. XOR wla tareeka best hai ye  
         
        unordered_map<int,int>mp;
        for(auto it:nums){
