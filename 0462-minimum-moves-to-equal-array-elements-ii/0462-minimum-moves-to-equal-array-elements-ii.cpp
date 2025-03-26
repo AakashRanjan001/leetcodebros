4f04998bdf95f9236fa32167fca3ce33cbc5ce06
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-03-26 17:49:29 +0530

    Time: 3 ms (30.04%), Space: 15.8 MB (50.27%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,17 +1,15 @@
+typedef long long ll;
 class Solution {
 public:
     int minMoves2(vector<int>& nums) {
+        
         sort(nums.begin(),nums.end());
-       //basic observation is needed
-       //difference ebetween the last and first element 
-
-    int n = nums.size();
-    int i=0;
-    int j = n-1;
- int sum =0;
-while(i<j){
-sum+= nums[j]-nums[i];
-}
-return sum;
+        ll n = nums.size();
+        ll oper =0;
+        ll median = nums[n/2];
+        for(auto i:nums){
+             oper+=abs(i-median);
+        }
+       return oper;
     }
 };
