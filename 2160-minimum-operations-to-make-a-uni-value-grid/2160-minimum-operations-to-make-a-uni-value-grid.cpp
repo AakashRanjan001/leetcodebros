917f3af06726517f805bee120a24617e444acfb0
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-03-26 17:24:19 +0530

    Time: 70 ms (5.77%), Space: 99.8 MB (6.38%) - LeetHub

---
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -0,0 +1,30 @@
+typedef long long ll;
+class Solution {
+public:
+    int minOperations(vector<vector<int>>& grid, int x) {
+         ll n = grid.size();
+          // how to find the target value it must be the emdian of the value 
+          vector<int>temp;
+          for(auto i:grid){
+             for(auto j:i){
+                 temp.push_back(j);
+             }
+          }
+          sort(temp.begin(),temp.end());
+
+          // for the case of -1 
+          for(auto i:temp){
+             if(abs(i-temp[0])%x !=0){
+                 return -1;
+             }
+          }
+            
+          ll median = temp[temp.size()/2];
+          int oper =0;
+          for(auto i:temp){
+             oper+=abs(i-median)/x;
+          }
+          return oper;
+
+    }
+};
