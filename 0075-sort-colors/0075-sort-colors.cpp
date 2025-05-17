4d59c40ecec8d2102007e0fa17d0233468c3cc44
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-05-17 09:08:45 +0530

    Time: 0 ms (100%), Space: 11.5 MB (78.6%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-          for(int i=0;i<nums.size()-1;i++){  //selection sort 
-
-    int minindex = i;  //we assume that first index is our minimum element
-    //For the inner loop in order to find min elemenyt corressponding to the value of i
-    for(int j=i+1;j<nums.size();j++){
-      if(nums[j]<nums[minindex]){
-        minindex = j;//if we found that j is muchminimum than arr[minindex] then store j
-      }       //new minimum then store it
-    }
-    //after finding the new minelemnt we will swap the corresponding value of i
-    swap(nums[i],nums[minindex]);
-  }
+        
+        int n = nums.size();
+        priority_queue<int , vector<int> , greater<int>>pq;
+        for(int i=0;i<n;i++){
+             pq.push(nums[i]);
+        }
+        nums.clear();
+        while(!pq.empty()){
+             nums.push_back(pq.top());
+             pq.pop();
+        }  
     }
 };
