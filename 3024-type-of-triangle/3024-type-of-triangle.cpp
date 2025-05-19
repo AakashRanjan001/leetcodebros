60e5cc9a1f8d53988ea4795ddaf97320bd890349
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-05-19 14:30:37 +0530

    Time: 0 ms (100%), Space: 23.1 MB (8.96%) - LeetHub

--- 3024-type-of-triangle/3024-type-of-triangle.cpp
+++ 3024-type-of-triangle/3024-type-of-triangle.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
-    bool isValid(vector<int>&nums){
-         int n = nums.size();
-         for(int i=0;i<n-2;i++){
-             if((nums[i]+ nums[i+1] > nums[i+2]) && (nums[i] + nums[i+2] > nums[i+1] )&& (nums[i+1] + nums[i+2] > nums[i])){
-                   return true;
-             }
-         }
-         return false;
+    bool isValid(vector<int>&nums, int i){
+        return (nums[i]+nums[i+1]>nums[i+2] && nums[i+1]+nums[i+2] > nums[i] && nums[i] + nums[i+2]>nums[i+1]);
     }
     string triangleType(vector<int>& nums) {
         int n = nums.size();
         string ans = "";
         
         for(int i=0;i<n-2;i++){
-             if(isValid(nums)){
+     if(isValid(nums,i)){
              if(nums[i] == nums[i+1] && nums[i+1] == nums[i+2] && nums[i] == nums[i+2]){
                  ans = "equilateral";
              }
