b14f244f253b04465daa124d6cf3794574d9dc6f
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-05-23 11:32:40 +0530

    Time: 0 ms (100%), Space: 126.6 MB (87.74%) - LeetHub

--- 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.cpp
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
+    typedef long long ll;
     long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
         
+        // < MY WRONG SOLUTION>!!!!!-------------------->
         // vector<int>temp = nums;
         // int n = nums.size();
         
@@ -17,27 +19,50 @@ public:
         // }
         // return sum;
 
-        vector<int>temp = nums;
-        for(auto i:edges){
-             int u = i[0];
-             int v = i[1];
+        // vector<int>temp = nums;
+        // for(auto i:edges){
+        //      int u = i[0];
+        //      int v = i[1];
              
-            int Xor_u = nums[u]^k;
-            if(Xor_u > nums[u]){
-                temp[u] = Xor_u;
-            }
-            int Xor_v = nums[v]^k;
-            if(Xor_v > nums[v]){
-                 temp[v] = Xor_v;
-            }
+        //     int Xor_u = nums[u]^k;
+        //     if(Xor_u > nums[u]){
+        //         temp[u] = Xor_u;
+        //     }
+        //     int Xor_v = nums[v]^k;
+        //     if(Xor_v > nums[v]){
+        //          temp[v] = Xor_v;
+        //     }
             
            
-        }
-        int sum =0;
-        for(int i=0;i<temp.size();i++){
-             sum+=temp[i];
-        }
-        return sum;
+        // }
+        // int sum =0;
+        // for(int i=0;i<temp.size();i++){
+        //      sum+=temp[i];
+        // }
+        // return sum;
+        ll sum =0;
+        ll countofXored = 0; // this is teh count of nodes how many are xored
+        ll minNuksaan = INT_MAX;
 
+        for(ll num:nums){
+              
+              int val = num^k;
+              if(val>num){
+                  countofXored++;
+                  sum+=val;
+              }
+              else{
+                 sum+=num;
+              }
+
+              minNuksaan = min((long long)minNuksaan , abs(val - num));
+        }
+
+        if(countofXored %2 ==0){
+             return sum;
+        }
+    
+             return sum - minNuksaan;
+    
     }
 };
