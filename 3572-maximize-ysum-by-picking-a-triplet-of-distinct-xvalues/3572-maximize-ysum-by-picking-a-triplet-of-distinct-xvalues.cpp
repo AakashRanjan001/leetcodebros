5e2bd922d5addf672a432e9ee81b015735994a56
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-06-11 10:37:44 +0530

    Time: 84 ms (81.48%), Space: 198.2 MB (69.49%) - LeetHub

--- 3572-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues/3572-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues.cpp
+++ 3572-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues/3572-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues.cpp
@@ -2,21 +2,48 @@ class Solution {
 public:
     int maxSumDistinctTriplet(vector<int>& x, vector<int>& y) {
            
-           unordered_map<int,int>m;
-           for(int i=0;i<x.size();i++){
-              if(m.find(x[i]) == m.end()){
-                   m[x[i]] = y[i];
-              }
-              else{
-                  m[x[i]] = max(m[x[i]] , y[i]);
-              }
-           }
-           if(m.size()<3)return -1;
-           int sum =0;
-           for(auto i:m){
-               sum+=i.second;
-           }
-         return sum;
-         
+        //    unordered_map<int,int>m;    // BY USING MAP
+        //    for(int i=0;i<x.size();i++){
+        //       if(m.find(x[i]) == m.end()){
+        //            m[x[i]] = y[i];
+        //       }
+        //       else{
+        //           m[x[i]] = max(m[x[i]] , y[i]);
+        //       }
+        //    }
+        //    vector<int>res;
+        //    for(auto i:m){
+        //      res.push_back(i.second);
+        //    }
+        //    if(res.size()<3)return -1;
+        //    sort(res.rbegin(),res.rend());
+        //    int sum =0;
+        //    for(int i=0;i<3;i++){
+        //      sum+=res[i];
+        //    }
+           
+        //  return sum;
+           
+        unordered_map<int,int>m;
+        for(int i=0;i<x.size();i++){
+             if(m.find(x[i]) == m.end()){
+                  m[x[i]] = y[i];
+             }
+             else{
+              m[x[i]] = max(m[x[i]], y[i]);
+             }
+        }
+        priority_queue<int>pq;
+        for(auto i:m){
+             pq.push(i.second);
+        }
+        if(pq.size()<3)return -1;
+        int sum =0;
+        for(int i=0;i<3;i++){
+             sum+=pq.top();
+             pq.pop();
+        }
+        return sum;
+
     }
 };
