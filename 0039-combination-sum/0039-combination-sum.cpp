60948e835204463954e0686297144e46bb01279c
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-06-18 22:03:28 +0530

    Time: 84 ms (10.58%), Space: 75.6 MB (5.01%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,26 +1,59 @@
+// class Solution {
+// public:
+//     void func(int ind,vector<int>& ds,int varsum,vector<int>&candidates,int target,vector<vector<int>>&ans){
+          
+//           if(ind>=candidates.size()){
+//              if(varsum == target){
+//                  ans.push_back(ds);
+//              }
+//              return;
+//           }
+//           //include 
+//           ds.push_back(candidates[ind]);
+//           varsum+=candidates[ind];
+//           func(ind+1,ds,varsum,candidates,target,ans);
+//           ds.pop_back();
+
+//           //exclude
+//           varsum-=candidates[ind];
+//           func(ind+1,ds,varsum,candidates,target,ans);
+
+
+//     }
+//     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+//          //whos esum is k 
+//          vector<vector<int>>ans;
+//          vector<int>ds;
+//          func(0,ds,0,candidates,target,ans);
+//          return ans;
+//     }
+// };
+
 class Solution {
 public:
-    void func(int ind, int target,vector<int>&arr,vector<int> &ds ,vector<vector<int>> &ans){
-         if(ind == arr.size()){
-             if(target == 0){
-                    ans.push_back(ds); // takes o(k);
-             }
-             return;
-         }
+    void func(int ind, vector<int> ds, int varsum, vector<int> candidates, int target, vector<vector<int>>& ans) {
+        if (ind >= candidates.size()) {
+            if (varsum == target) {
+                ans.push_back(ds);
+            }
+            return;
+        }
 
-         // pick conditiomn 
-         if(arr[ind]<=target){
-             ds.push_back(arr[ind]);
-             func(ind,target - arr[ind],arr,ds,ans);
-             ds.pop_back(); // while backtracking we use this condition and pop_back 
-         }
-         //not pick
-         func(ind+1,target,arr,ds,ans);
+        // include
+        if (varsum + candidates[ind] <= target) {
+            ds.push_back(candidates[ind]);
+            func(ind, ds, varsum + candidates[ind], candidates, target, ans);
+            ds.pop_back(); // not necessary due to pass-by-value, but kept for symmetry
+        }
+
+        // exclude
+        func(ind + 1, ds, varsum, candidates, target, ans);
     }
+
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>>ans;
-        vector<int>ds;
-        func(0,target,candidates,ds,ans);
+        vector<vector<int>> ans;
+        vector<int> ds;
+        func(0, ds, 0, candidates, target, ans);
         return ans;
     }
 };
