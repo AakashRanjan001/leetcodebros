fa63d5f0000dfb0c8af16b44a81302aac492802d
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-06-20 19:53:49 +0530

    Time: 3 ms (38.67%), Space: 10.2 MB (99.88%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,28 +1,23 @@
-
 class Solution {
 public:
-    void solve(int i, vector<int>& ds, vector<int>& nums, set<vector<int>>& st, int n) {
-        if (i == n) {
-            // sort(ds.begin(),ds.end());
-            st.insert(ds);
-            return;
-        }
-         // Not picking the element
-        solve(i + 1, ds, nums, st, n);
-        // Picking the element
-        ds.push_back(nums[i]);
-        solve(i + 1, ds, nums, st, n);
-        ds.pop_back();
+    void func(int ind , vector<int>& ds, vector<int> &nums, vector<vector<int>>& ans , int n){
+           
+             ans.push_back(ds);
+             
+           for(int i=ind;i<nums.size();i++){
+             if(i>ind && nums[i] ==nums[i-1])continue;
+             ds.push_back(nums[i]);
+             func(i+1,ds,nums,ans,n);
+             ds.pop_back();
+           }
+          
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        int n = nums.size();
-        set<vector<int>> st;
-        vector<int> ds;
-        sort(nums.begin(),nums.end());
-        solve(0, ds, nums, st, n);
-        // Convert set to vector
-
-        vector<vector<int>> ans(st.begin(), st.end());
-        return ans;
+         int n =nums.size();
+         sort(nums.begin(),nums.end());
+         vector<vector<int>>ans;
+         vector<int>ds;
+         func(0,ds,nums,ans,n);
+         return ans;
     }
 };
