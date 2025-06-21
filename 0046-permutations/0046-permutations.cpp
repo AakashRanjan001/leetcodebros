d3b32f433822140973ecf27098f20331ee7e5e5a
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-06-21 11:49:15 +0530

    Time: 0 ms (100%), Space: 10.4 MB (97.89%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,30 +1,54 @@
+// class Solution {
+// public:
+//     void func(vector<int> &ds, vector<int>& nums, vector<bool>&hash , vector<vector<int>>&ans){
+//          if(ds.size() == nums.size()){
+//              ans.push_back(ds);
+//              return;
+//          }
+//          for(int i=0;i<nums.size();i++){
+//              if(hash[i] == 0){
+//                  ds.push_back(nums[i]);
+//                  hash[i] = 1;
+//                  func(ds,nums,hash,ans);
+//                  ds.pop_back();
+//                  hash[i] = 0;
+//              }
+             
+             
+//          }
+//     }
+//     vector<vector<int>> permute(vector<int>& nums) {
+//          int n = nums.size();
+//          vector<bool>hash(n);
+//          vector<int>ds;
+//          vector<vector<int>>ans;
+//          for(int i=0;i<n;i++){
+//              hash[i] = 0;
+//          }
+//          func(ds,nums,hash,ans);
+//          return ans;
+
+//     }
+// };
 class Solution {
 public:
-   void permutation(vector<int>&nums , vector<int>&ds , vector<vector<int>>&ans , vector<int>&freq){
-        
-        //base case 
-        if(ds.size() == nums.size() ){
-             ans.push_back(ds);
+    void func(int ind , vector<int>& nums,vector<vector<int>>& ans){
+         if(ind == nums.size()){
+             ans.push_back(nums);
              return;
-        }
-        for(int i=0;i<nums.size();i++){
-             if(freq[i] == 0){
-                 freq[i] = 1;
-                 ds.push_back(nums[i]);
-                 permutation(nums,ds,ans,freq);
-                 freq[i] = 0;
-                 ds.pop_back();
-             }
-        }
-   }
- 
-    vector<vector<int>> permute(vector<int>& nums) {
-         vector<vector<int>>ans;
-         vector<int>ds;
-         vector<int>freq(nums.size(),0);
-         
+         }
 
-         permutation(nums,ds,ans,freq);
+         for(int i=ind;i<nums.size();i++){
+             swap(nums[ind],nums[i]);
+             func(ind+1,nums,ans);
+             swap(nums[ind],nums[i]);// while coming back make sure to re swap it
+             
+         }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+         int n = nums.size();
+         vector<vector<int>>ans;
+         func(0,nums,ans);
          return ans;
     }
 };
