fd8e576cdf5d5a7f6457309a2fe93cf9afae1821
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-06-22 09:08:26 +0530

    Time: 0 ms (100%), Space: 9.7 MB (70.3%) - LeetHub

--- 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.cpp
+++ 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.cpp
@@ -3,11 +3,11 @@ public:
     vector<string> divideString(string s, int k, char fill) {
        // pehle string ko padd kar lete hai if the size is not divisible by k
 
-       
+          int n = s.size();
        while(s.size()%k!=0){
           s+=fill;
        }
-       int n = s.size();
+    
        vector<string>ans;
        for(int i=0;i<n;i+=k){
           ans.push_back(s.substr(i,k));
