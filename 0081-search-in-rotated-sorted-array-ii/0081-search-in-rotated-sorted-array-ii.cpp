651bbaa33cfd906b7a169a1fd05d676b577b287d
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-06-26 11:58:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,41 +1,42 @@
 class Solution {
-public:
+public: 
+    int indexPresent(vector<int>& nums, int target, int n){
+          int l = 0;
+          int h = n-1;
+
+          while(l<=h){
+             int mid = l + (h-l)/2;
+             if(nums[mid] == target)return mid;
+             if(nums[mid] == nums[l] && nums[mid] == nums[h]){
+                 l = mid+1; // to avoid the duplicates we can shrink it 
+                 h = mid-1;
+                 continue;
+             }
+             //left Sorted 
+             if(nums[mid]>=nums[l]){
+                 if(target>=nums[l] && target<nums[mid]){
+                     h = mid-1;
+                 }
+                 else{
+                     l = mid+1;
+                 }
+             }
+             else if(nums[mid]<=nums[h]){
+                 if(target>nums[mid] && target<=nums[h]){
+                     l = mid+1;
+                 }
+                 else{
+                     h = mid-1;
+                 }
+             }
+          }
+          return -1;
+    }
     bool search(vector<int>& nums, int target) {
-        
-        int n = nums.size();
-        int s =0;
-        int e = n-1;
-
-        while(s<=e){
-           int mid = s+(e-s)/2;
-
-            if(nums[mid]== target)return true;
-
-            //imp part
-            if(nums[s]== nums[mid] && nums[e] == nums[mid]){
-                s++;
-                e--;
-                continue;
-            }
-
-            //left sorted 
-            else if(nums[s]<=nums[mid]){
-                if(target<=nums[mid] && target>=nums[s]){
-                    e = mid-1;
-                }
-                else{
-                    s = mid+1;
-                }
-            }
-            else{
-                if(target>=nums[mid] && target<= nums[e]){
-                    s = mid+1;
-                }
-                else{
-                    e = mid-1;
-                }
-            }
-        }
-        return false;
+         int n = nums.size();
+         int ans = indexPresent(nums,target,n);
+         
+         bool result = (ans == -1)? false:true;
+         return result;
     }
 };
