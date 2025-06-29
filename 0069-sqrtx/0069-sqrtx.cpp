138aca7005c49aac3aae3c0b02a472c0912c2510
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-06-29 19:06:37 +0530

    Time: 0 ms (100%), Space: 8.6 MB (50.86%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,37 @@
 class Solution {
 public:
-    int mySqrt(int x) {
-         return floor(sqrt(x));
+    int mySqrt(int x) { 
+        //  return floor(sqrt(x));  // TAREEKAA 1------>
+
+// TAREEKAAA 2------>>>
+        // int ans = 1;
+        // if(x ==0)return 0;
+        // for(int i=1;i<=x;i++){
+        //       if(i*i<=x){
+        //          ans = i;
+        //       }
+        //       else{
+        //          break;
+        //       }
+        // }
+        // return ans;
+
+
+  // TAREEKAA 3 ----->
+   // we can reduce teh search space by using the binary search 
+    int low = 1;
+    int high = x;
+    int ans = 0;
+    while(low<=high){
+         int mid =  low + (high - low)/2;
+         if(1LL* mid*mid<=x){ // 1LL is long long which is used to avoid integer overflow
+            ans = mid;
+            low = mid+1;
+         }
+         else{
+             high = mid-1;
+         }
+    }   
+     return ans;
     }
 };
