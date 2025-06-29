7004b6ceb813793f81f625262c803ef801f6500f
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-06-29 20:47:06 +0530

    Time: 0 ms (100%), Space: 8.5 MB (70.41%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,40 @@
+// class Solution {
+// public:
+//     double power(double &a, int &b){
+//          double res =1;
+//          while(b>0){
+//              if(b%2 == 1){
+//                  res = res* a;
+//              }
+//              a = a*a;
+//              b>>=1;
+//          }
+//          double ans = (b>=0)? res : double(1/res);
+//          return ans;
+//     }
+//     double myPow(double x, int n) {
+//          return power(x,n); 
+//     }
+// };
+
 class Solution {
 public:
+    double power(double a, long long b) {
+        double res = 1.0;
+        while (b > 0) {
+            if (b % 2 == 1) res *= a;
+            a *= a;
+            b >>= 1;
+        }
+        return res;
+    }
+
     double myPow(double x, int n) {
-         return double(pow(x,n));
+        long long b = n;  // Use long long to safely handle INT_MIN
+        if (b < 0) {
+            x = 1.0 / x;
+            b = -b;
+        }
+        return power(x, b);
     }
 };
