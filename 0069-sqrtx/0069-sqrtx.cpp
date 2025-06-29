413d48b28eb8d96080232cc6e471daa97b5f6a61
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-06-29 18:53:48 +0530

    Time: 0 ms (100%), Space: 8.7 MB (14.54%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,28 +1,6 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int s =0;
-        int e = x;
-    
-        
-        
-        long long mid = s + (e-s)/2;
-        for(;s<=e;){
-            if(mid*mid==x){
-                return mid;
-            }
-            else if(mid*mid<x){
-                
-                s = mid +1;
-            }
-            else{
-                e = mid -1;
-            }
-            mid = s + (e-s)/2;        
-        
-    
-        }
-        return e;
+         return floor(sqrt(x));
     }
-    
 };
