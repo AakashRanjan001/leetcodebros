b3e6ca2d911d326c87a63e1fe2fe5be72d470cb6
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-07-04 11:06:24 +0530

    Time: 0 ms (100%), Space: 7.9 MB (90.89%) - LeetHub

--- 3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.cpp
+++ 3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     char kthCharacter(int k) {
-          string res = "a";
-          while(res.length()<k){
-             int n = res.length();
-             for(int i=0;i<n;i++){
-                 char ch = res[i] == 'z'?'a' : res[i]+1;
-                 res.push_back(ch);
-             }
-          }
-          return res[k-1];
+
+        // TIME COMPLEXITY ---> O(K)
+        //   string res = "a";
+        //   while(res.length()<k){
+        //      int n = res.length();
+        //      for(int i=0;i<n;i++){
+        //          char ch = res[i] == 'z'?'a' : res[i]+1;
+        //          res.push_back(ch);
+        //      }
+        //   }
+        //   return res[k-1];
+
+        // BETTER SOLUTION 
+        int shift = __builtin_popcount(k-1);
+        return 'a'+shift;
+
     }
 };
