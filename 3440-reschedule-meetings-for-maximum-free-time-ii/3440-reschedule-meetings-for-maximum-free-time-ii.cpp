0a34516db3a6ca6a877dbcd2a3239e335705a4df
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-07-12 19:21:29 +0530

    Time: 19 ms (29.62%), Space: 150.3 MB (33.26%) - LeetHub

--- 3440-reschedule-meetings-for-maximum-free-time-ii/3440-reschedule-meetings-for-maximum-free-time-ii.cpp
+++ 3440-reschedule-meetings-for-maximum-free-time-ii/3440-reschedule-meetings-for-maximum-free-time-ii.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
-    int maxFreeTime(int eventTime, vector<int>& startTime,
-                    vector<int>& endTime) {
-        vector<int> gap(1, startTime[0]);
-        for (int i = 1; i < startTime.size(); ++i)
-            gap.push_back(startTime[i] - endTime[i - 1]);
-        gap.push_back(eventTime - endTime.back());
+    int maxFreeTime(int eventTime, vector<int>& startTime, vector<int>& endTime) {
+         // ye bhi arrau aur  greedy ka ho skat ahia 
+         vector<int>freeArr;
+         freeArr.push_back(startTime[0] - 0);
+         for(int i=1;i<startTime.size();i++){
+             int gap = startTime[i] - endTime[i-1];
+             freeArr.push_back(gap); 
+         }
+         freeArr.push_back(eventTime - endTime[endTime.size()-1]);
 
-        vector<int> largestRight(gap.size(), 0);
-        for (int i = gap.size() - 2; i >= 0; --i)
-            largestRight[i] = max(largestRight[i + 1], gap[i + 1]);
+         int n = freeArr.size();
+         //case1 : jab event ko dusre event ke uss parr karna hai either left or right
+          // case 2 : jab evenet ko adjacent right ya left mei shift karna hai , no change in the relative order in between the evenets 
+          vector<int>maxleftFree(n,0);
+          vector<int>maxrightFree(n,0);
 
-        int ans = 0, largestLeft = 0;
-        for (int i = 1; i < gap.size(); ++i) {
-            int curGap = endTime[i - 1] - startTime[i - 1];
-            if (curGap <= max(largestLeft, largestRight[i]))
-                ans = max(ans, gap[i - 1] + gap[i] + curGap);
-            ans = max(ans, gap[i - 1] + gap[i]);
-            largestLeft = max(largestLeft, gap[i - 1]);
-        }
-        return ans;
+          for(int i=n-2;i>=0;i--){
+             maxrightFree[i] = max(maxrightFree[i+1] , freeArr[i+1]);
+          }
+          for(int i = 1;i<n;i++){
+             maxleftFree[i] = max(maxleftFree[i-1],freeArr[i-1]);
+          }
+
+          int res =0;
+          for(int i=1;i<n;i++){
+             int curreventTime = endTime[i-1] - startTime[i-1]; 
+             // case 1 : moving completeley out 
+             if(curreventTime <= max(maxleftFree[i-1],maxrightFree[i])){
+                  res = max(res , freeArr[i-1] + curreventTime + freeArr[i]);
+             } 
+
+             // case 2 : shift left or right adjacnet 
+              res = max(res, freeArr[i-1]+freeArr[i]);
+          }
+          return res;
     }
 };
