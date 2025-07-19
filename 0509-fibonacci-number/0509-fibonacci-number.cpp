a221d1ef88d4e16a6e63a8b3d59bb81590b92ddf
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-07-19 15:59:49 +0530

    Time: 0 ms (100%), Space: 8.2 MB (17.35%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,72 +1,64 @@
-class Solution { // Recursive solution 
-public:
-//    int getRecSolve(int n){
-//        // base case
-//         if(n == 0 || n==1){
-//              return n;
-//         }
-//         int ans = getRecSolve(n-1) + getRecSolve(n-2);
-//         return ans;
-//    }
-//    int topDownSolve(int n , vector<int>&dp){
-//         // base case
-//         if(n == 0 || n == 1){
-//              return n;
-//         }
-//         //step 3: eke aur condition likhna hai 
-//         if(dp[n]!=-1){ // matlab dp[n] mei kuch value aaya hai 
-//                           // check if answer already exist 
-//              return dp[n];
-//         } 
-//         // function ka naam badalna yaha mai galti karta hu 
-//         // step two: jo answer hai woh return karoo replace ans with dp[n]
-//         dp[n] = topDownSolve(n-1,dp) + topDownSolve(n-2,dp);
-//         return dp[n];
-//    }
-    // int bottomUpSolve(int n){
-    //      // step 1:
-    //       // creat ea DP array
-    //       vector<int>dp(n+1 , -1);
+class Solution {
+public: 
+     int solve(int n){
+         //step 1: create a dp array 
+         vector<int>dp(n+1,-1);
 
-    //       // step 2:base casejo tumne top down mei likha tha usko upgrade karo
-    //       dp[0] = 0;
-    //       if(n==0)return dp[0];
-    //       dp[1] = 1;
-    //       if(n==1)return dp[1];
+         //step 2; base case mei dekho by looking at the top down condition
+         dp[0] = 0;
+          if( n == 0)return dp[0];
+         dp[1] = 1;
+          if( n == 1)return dp[1];
+        
+        
+    
 
-    //       //step 3: 
-    //       // perform the iterative approach // ulta as chalenge of top down
-    //       for(int i=2;i<=n;i++){
-    //          dp[i] = dp[i-1] + dp[i-2];
-    //       }
-    //       return dp[n];
-
-    // }
-    int spaceOptSolve(int n){
-         
-         //step2: base case
-         int prev2 =0;
-         int prev1 =1 ;
-
-// yaha mei galti hui
-      if(n==0)return prev2;
-      if(n==1)return prev1;
-
-         int curr;
+         //step 3: itertaive ulta as compared to the memoiszaryion
          for(int i=2;i<=n;i++){
-             curr = prev2 + prev1;
-             prev2 = prev1;
-             prev1 = curr;
+              dp[i] = dp[i-1] + dp[i-2];
          }
-         return curr;
-    }
+         return dp[n];
 
-   
+
+     }
+    // int solve(int n, vector<int>& dp){
+    //      if(n ==1 || n==0)return n;
+
+    //      if(dp[n]!=-1)return dp[n];
+
+    //      dp[n] = solve(n-1,dp) + solve(n-2,dp);
+    //      return dp[n];
+        
+    // }
     int fib(int n) {
-    //  return bottomUpSolve(n);
-    return spaceOptSolve(n);
-     
+         // APPROACH1 ----> normal For loop of O(N)
+        // if(n == 0|| n==1)return n;
+         
+        //  int a = 0;
+        //  int b  =1;
 
-    }
+        //  for(int i = 2;i<=n;i++){
+              
+        //  int sum = a+b;
+        //  a = b;
+        //  b = sum;
+        //  }
+        //  return b;
+  
+
+       // APPROACH 2 ---> Recursive Approach O(2^N)
+        // if(n ==1 || n ==0)return n;
+        // return fib(n-1)+fib(n-2);
+
+
+        //Approach 3 ---->DP with Memo
+        //  vector<int>dp(n+1,-1);
+        //  int ans = solve(n,dp);
+        //  return ans;
+
+
+        int ans = solve(n);
+        return ans;
+    } 
 
 };
