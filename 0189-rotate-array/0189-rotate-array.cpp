a56012de8281e5f2735257f578c317f3a6eab950
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-07-25 11:13:48 +0530

    Time: 0 ms (100%), Space: 29.6 MB (36.88%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,25 +1,9 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        
-    //      int n = nums.size();
-    //      k = k%n;
-    //      vector<int>concatenate(2*n);
-    //      for(int i=0;i<n;i++){
-    //          concatenate[i] = nums[i];
-    //          concatenate[i+n] = nums[i];
-    //      }
-    //     //  for(int i=0;i<2*n;i++){
-    //     // cout<<concatenate[i]<<"";
-    //     //  }
-
-    //    for(int i=0;i<n;i++){
-    //        nums[i] = concatenate[n-k +i];
-    //    }
-
-    reverse(nums.begin(),nums.end());
-    reverse(nums.begin(),nums.begin()+k);
-    reverse(nums.begin()+k,nums.end());
-        
+         k = k% nums.size();
+         reverse(nums.begin(),nums.end());
+         reverse(nums.begin(), nums.begin() + k);
+         reverse(nums.begin()+k,nums.end());
     }
 };
