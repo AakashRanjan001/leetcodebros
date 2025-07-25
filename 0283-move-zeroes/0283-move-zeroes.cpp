4cc4d72341afe5d17fb060bfd0f0c199e8bd539b
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-07-25 14:32:47 +0530

    Time: 0 ms (100%), Space: 23.9 MB (53.16%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,32 +1,38 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-    //     int n = nums.size();
-    //     vector<int>temp;
-    //     int cnt_zero =0;
-    //     for(int i=0;i<n;i++){
-    //         if(nums[i]!=0){
-    //             temp.push_back(nums[i]);  
-    //         }
-    //         else{
-    //             cnt_zero++;
-    //         }
-    //     }
+        //  int n = nums.size();
+        //  vector<int>vec;
+        //  int cnt_zero =0;
+        //  for(int i=0;i<n;i++){
+        //      if(nums[i]!=0){ 
+        //         vec.push_back(nums[i]);
+        //      }
+        //      else{
+        //          cnt_zero++;
+        //      }
+        //  }
+        //  while(cnt_zero>0){
+        //      vec.push_back(0);
+        //  }
+        //  return vec;
 
-    //  while(cnt_zero--){
-    //     temp.push_back(0);
-    //  }
-
-    int n = nums.size();
-    int nonzeropointer =0;
-    for(int i=0;i<n;i++){
-        if(nums[i]!=0){
-             int temp = nums[i];
-             nums[i] = nums[nonzeropointer];
-             nums[nonzeropointer] = temp;
-             nonzeropointer++;
+        int n = nums.size();
+        int i=-1;
+        for(int j=0;j<n;j++){
+             if(nums[j] == 0){ 
+                i = j;
+                break;
+             }
         }
-    }
-  
+         if (i == -1) return;
+
+        for(int j = i+1;j<n;j++){
+             if(nums[j]!=0){
+                 swap(nums[i],nums[j]);
+                 i++;
+             }
+        }
+
     }
 };
