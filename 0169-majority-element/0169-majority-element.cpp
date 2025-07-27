bea453c5a6ca90e8cec1f2dc3405bc908f5f8cf2
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-07-27 13:15:48 +0530

    Time: 0 ms (100%), Space: 28.3 MB (6.95%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+         int n = nums.size();
+         int val = (int)n/2; // by default floor value hoga 
+
+         unordered_map<int,int>mp;
+         for(auto i:nums){
+             mp[i]++;
+         }
+       
+         for(auto it:mp){
+             if(it.second>val){
+                 return it.first;
+                 break;
+             }
+         }
+         return 0;
         
-        map<int,int>mpp;
-        for(int i=0;i<nums.size();i++){
-            mpp[nums[i]]++;
-        }
-        for(auto it:mpp){
-            if(it.second>nums.size()/2){
-                return it.first;
-            }
-        }
-        return -1;
     }
 };
