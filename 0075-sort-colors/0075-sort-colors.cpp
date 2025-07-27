36a7f1442301d7dbcce7ededf6dc2d9f05903a6d
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-07-27 11:36:11 +0530

    Time: 0 ms (100%), Space: 11.6 MB (48.76%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        
-        int n = nums.size();
-        priority_queue<int , vector<int> , greater<int>>pq;
-        for(int i=0;i<n;i++){
+         //priority Quwue
+         int n = nums.size();
+         priority_queue<int , vector<int>, greater<int>>pq;
+         for(int i =0;i<n;i++){
              pq.push(nums[i]);
-        }
-        nums.clear();
-        while(!pq.empty()){
+         }
+         nums.clear();
+         while(!pq.empty()){
              nums.push_back(pq.top());
              pq.pop();
-        }  
+         }
+       
+
     }
 };
