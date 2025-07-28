dcf7da13ee6ca4880288abe68c4ac7e255c20ae5
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-07-28 10:54:55 +0530

    Time: 0 ms (100%), Space: 71.7 MB (52.2%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,23 +1,45 @@
-
-
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int maxSum = INT_MIN;
-        int currentSum = 0;
+        //  int n = nums.size(); // O(N^3)
+        //  //brute force generate all the subarrays 
+        //  int sum =0;
+        //  int maxi = INT_MIN;
+        //  for(int i=0;i<n;i++){
+        //      for(int j=i;j<n;j++){ 
+        //        sum =0;
+        //         for(int k=i;k<=j;k++){
+        //              sum+=nums[k];
+        //         }
+        //         maxi = max(sum, maxi);
+        //      }
+        //  }
+        //  return maxi;
+         
+
+        //  int n = nums.size();
+        //  int maxi = INT_MIN;
+        //  for(int i=0;i<n;i++){
+        //     int sum =0;
+        //      for(int j=i;j<n;j++){
+        //          sum+=nums[j];
+        //          maxi = max(maxi,sum);
+        //      }
+        //  }
+        //  return maxi;
         
-        for (int i = 0; i < nums.size(); i++) {
-            currentSum += nums[i];
-            
-            if (currentSum > maxSum) {
-                maxSum = currentSum;
-            }
-            
-            if (currentSum < 0) {
-                currentSum = 0;
-            }
+        // KADANE'S Algo 
+        int sum = 0, maxi = INT_MIN;
+        for(int i=0;i<nums.size();i++){
+              sum+=nums[i];
+              maxi = max(maxi,sum);
+
+              if(sum<0)sum =0;
+              else if(sum>maxi){
+                 maxi = sum;
+              }
         }
-        
-        return maxSum;
+        return maxi;
+
     }
 };
