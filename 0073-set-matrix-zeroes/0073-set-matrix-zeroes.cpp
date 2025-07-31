25e2cf2856a087c139817ce636afcac7ad3e32d8
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-07-31 11:56:52 +0530

    Time: 1 ms (15.64%), Space: 19 MB (5.97%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        
-        vector<vector<int>> ans = matrix;  // Initialize ans with same dimensions
+         vector<vector<int>>temp = matrix;
+         int n = matrix.size();
+         int m = matrix[0].size();
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                 if(matrix[i][j] == 0){
+                     for(int k=0;k<m;k++){
+                         temp[i][k] = 0;
+                     }
+                     for(int k=0;k<n;k++){
+                         temp[k][j] = 0;
+                     }
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(matrix[i][j] == 0) {
-                    for(int k = 0; k < m; k++) { // row wise apon zero daal rhe 
-                        ans[i][k] = 0;
-                    }
-                    for(int k = 0; k < n; k++) { // col wiae apon zero daal rhe 
-                        ans[k][j] = 0;
-                    }
-                }
+                 }
             }
-        }
-
-        matrix = ans;
+         }
+          matrix = temp;
+       
     }
 };
