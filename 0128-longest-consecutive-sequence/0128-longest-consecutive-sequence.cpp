d0a778e4b29ff1ad0b18ff6fcacc12fbf8e81194
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-07-31 10:27:58 +0530

    Time: 122 ms (16.29%), Space: 91.2 MB (9.78%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,25 +1,53 @@
+// class Solution {
+// public:
+//     int longestConsecutive(vector<int>& nums) {
+//         int n = nums.size();
+//         int maxlen = 1;
+//         if(n == 0)return 0;
+
+//         unordered_set<int>s(nums.begin(),nums.end());
+//         auto prev = s.begin();
+//         auto it = next(s.begin());
+//         int cnt = 1;
+
+//         while(it!=s.end()){
+//              if(*prev +1 == *it){
+//                  cnt++;
+//              }
+//              else{
+//                  cnt =1;
+//              }
+//              maxlen = max(maxlen,cnt);
+//              prev = it;
+//              it++;
+//         }
+//         return maxlen;
+//     }
+// };
+
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        
         int n = nums.size();
-        if(n==0)return 0;
-        int longest =0;
+        if(n == 0) return 0;
 
-        set<int>st;
-       for(int i=0;i<n;i++){
-        st.insert(nums[i]);
-       }
-       int cnt =1;
-       for(auto i: st){
-          if(st.find(i+1)!=st.end()){
-            cnt++;
-          }
-          else if(cnt>longest){
-            longest = cnt;
-          }
-       }
-       return longest;
-  
+        set<int> s(nums.begin(), nums.end());
+
+        int longest = 1, cnt = 1;
+        auto prev = s.begin();
+        auto it = next(s.begin());
+
+        while(it != s.end()) {
+            if(*it == *prev + 1) {
+                cnt++;
+            } else {
+                cnt = 1;
+            }
+            longest = max(longest, cnt);
+            prev = it;
+            ++it;
+        }
+
+        return longest;
     }
 };
