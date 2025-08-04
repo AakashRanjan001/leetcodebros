9a6da57875f5419a9d9edc9ae21e77b460f91f4a
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-08-04 10:25:45 +0530

    Time: 11 ms (23.74%), Space: 26.9 MB (13.85%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-         int times = nums.size()/3;
-         set<int>ans;
+        //  int times = nums.size()/3;
+        //  set<int>ans;
      
-         for(int i=0;i<nums.size();i++){
-            int cnt =1;
-             for(int j=i+1;j<nums.size();j++){
-                 if(nums[i] == nums[j]){
-                     cnt++;
-                 }
+        //  for(int i=0;i<nums.size();i++){
+        //     int cnt =1;
+        //      for(int j=i+1;j<nums.size();j++){
+        //          if(nums[i] == nums[j]){
+        //              cnt++;
+        //          }
+        //      }
+        //       if(cnt>times)ans.insert(nums[i]);
+        //  }
+        //  vector<int>val(ans.begin(),ans.end());
+        //  return val;
+
+
+        map<int,int>m;
+        vector<int>ans;
+        int n = nums.size();
+        for(auto i:nums){
+             m[i]++;
+        }
+        for(auto it:m){
+             if(it.second>(n/3)){
+                 ans.push_back(it.first);
              }
-              if(cnt>times)ans.insert(nums[i]);
-         }
-         vector<int>val(ans.begin(),ans.end());
-         return val;
+        }
+        return ans;
     }
 };
