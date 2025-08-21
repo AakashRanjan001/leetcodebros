33a63d6bc826070d55ba22b8f99c2b9cb50d659c
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-08-21 16:22:47 +0530

    Time: 0 ms (100%), Space: 15 MB (76.51%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -1,93 +1,47 @@
-// /**
-//  * Definition for a binary tree node.
-//  * struct TreeNode {
-//  *     int val;
-//  *     TreeNode *left;
-//  *     TreeNode *right;
-//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-//  * };
-//  */
-// class Solution {
-// public:
-//     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-//         vector<vector<int>>ans;
-//         bool LtoRdir = true; // for left to right maan liya 
-//         queue<TreeNode*>q;
-//         q.push(root);
-
-//         while(!q.empty()){
-//             // abb width kya hoga ek vector ka 
-//             int width = q.size();
-//             vector<int>oneLevel(width);
-//             for(int i=0;i<width;i++){
-//                  TreeNode* temp = q.front();
-//                  q.pop();
-//                  int index ;
-//                  if(LtoRdir == true){
-//                       index =i;
-//                  }
-//                  else{ // agar LtoRdir is false matlab reverse order mei print karna hai toh index ko peeche se start karna padega 
-//                     index = width - i-1; // agar 
-//                  }
-//                 oneLevel[index] = temp->val;
-//                 if(temp->left!=NULL){
-//                     q.push(temp->left);
-//                 }
-//                 if(temp->right!=NULL){
-//                     q.push(temp->right);
-//                 }
-                 
-//             }
-//           // har ek level ke baad toggle karna hai
-//           LtoRdir = !LtoRdir;
-//           ans.push_back(oneLevel);
-//         }
-//         return ans;
-//     }
-// };
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        
-        vector<vector<int>> ans;
-        if (root == NULL) return ans;  // Handle empty tree case
-        
-        bool LtoRdir = true; // Start from left to right
-        queue<TreeNode*> q;
-        q.push(root);
+        vector<vector<int>>ans;
+         if(root == NULL)return ans;
+          queue<TreeNode*>q;
+          q.push(root);
+          int level =0;
 
-        while (!q.empty()) {
-            int width = q.size();
-            vector<int> oneLevel(width);
+          while(!q.empty()){
+                 vector<int>var;
+               
+               int size = q.size();
+               for(int i=0;i<size;i++){
+                    TreeNode* temp = q.front();
+                     q.pop();
+                      
+                     var.push_back(temp->val);
+
+                     if(temp->left!=NULL)q.push(temp->left);
+                     if(temp->right!=NULL)q.push(temp->right);
+               }
+                
+               ans.push_back(var);
             
-            for (int i = 0; i < width; i++) {
-                TreeNode* temp = q.front();
-                q.pop();
-                
-                int index;
-                if (LtoRdir) {
-                    index = i;
-                } else {
-                    index = width - i - 1;
-                }
-                
-                oneLevel[index] = temp->val;
-                
-                if (temp->left != NULL) {
-                    q.push(temp->left);
-                }
-                if (temp->right != NULL) {
-                    q.push(temp->right);
-                }
-            }
-            
-            // Toggle direction after each level
-            LtoRdir = !LtoRdir;
-            ans.push_back(oneLevel);
-        }
-        
-        return ans;
+          }
+          for(int i=0;i<ans.size();i++){
+              if(i%2!=0){
+                 reverse(ans[i].begin(),ans[i].end());
+              }
+          }
+          return ans;
+      
+          
     }
 };
