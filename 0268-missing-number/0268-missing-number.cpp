c84abfc8d35ff3adece848a975905c91611db533
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-09-01 18:49:48 +0530

    Time: 0 ms (100%), Space: 21.9 MB (13.87%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -15,14 +15,24 @@ public:
         //  }
         //  return 0;
 
-        int n = nums.size();
-        int T_sum = 0;
-        for(int i=0;i<=n;i++){
-             T_sum+=i;
-        }
-        int arr_sum = accumulate(nums.begin(),nums.end(), 0);
-        return T_sum - arr_sum;
-
+        // int n = nums.size();
+        // int T_sum = 0;
+        // for(int i=0;i<=n;i++){
+        //      T_sum+=i;
+        // }
+        // int arr_sum = accumulate(nums.begin(),nums.end(), 0);
+        // return T_sum - arr_sum;
+       
+       int n = nums.size();
+       int XOR1 = 0;
+       for(int i=1;i<=n;i++){
+          XOR1^=i;
+       }
+       int XOR2 = 0;
+       for(int i=0;i<n;i++){
+          XOR2^=nums[i];
+       }
+       return XOR1^XOR2;
      
         
 
