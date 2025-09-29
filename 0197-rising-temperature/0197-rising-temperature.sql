cdd5541580ddf6a2d9dde59fa479183021a70d63
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-09-29 18:26:53 +0530

    Time: 515 ms (32.45%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,14 +1,19 @@
-# Write your MySQL query statement below
--- this is a good question 
--- OUTER QUERY will have an eye on the current date and 
--- INNER QUERY will have an eye on previos date (yesterday to be specific)
+-- # Write your MySQL query statement below
+-- -- this is a good question 
+-- -- OUTER QUERY will have an eye on the current date and 
+-- -- INNER QUERY will have an eye on previos date (yesterday to be specific)
 
-Select Today.id from Weather as Today
-WHERE EXISTS
+-- Select Today.id from Weather as Today
+-- WHERE EXISTS
 
-     -- NESTED QUERY returns true or false
-    ( SELECT 1 FROM Weather as Yesterday
-     WHERE 
-     Yesterday.temperature < Today.temperature
-     AND
-     Today.recordDate - Yesterday.recordDate = 1)
+--      -- NESTED QUERY returns true or false
+--     ( SELECT 1 FROM Weather as Yesterday
+--      WHERE 
+--      Yesterday.temperature < Today.temperature
+--      AND
+--      Today.recordDate - Yesterday.recordDate = 1)
+
+
+SELECT w1.id
+FROM Weather w1, Weather w2
+WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 AND w1.temperature > w2.temperature;
