551a1f0bd2677f1d408d5ca3466204c40590b712
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-10-04 11:44:09 +0530

    Time: 11 ms (94.91%), Space: 81.4 MB (75.5%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,75 +1,32 @@
 class Solution {
 public:
-vector<int> prevSmallerElement(vector<int>& input) {
-        stack<int> s;
-        s.push(-1);
-        vector<int> ans(input.size());
-
-        //left to right -> prev smaller
-        for(int i=0; i<input.size(); i++) {
-                int curr = input[i];
-
-                //apka answer stack me 
-                while(s.top() != -1 && input[s.top()] >= curr) {
-                        s.pop();
-                }
-
-                //chotta element mil chuka h -> ans store
-                ans[i] = s.top();
-
-                //push krdo curr element ko
-                s.push(i);
-        }
-        return ans;
-}
-
-vector<int> nextSmaller(vector<int> &input) {
-  stack<int> s;
-  s.push(-1);
-  
-  vector<int> ans(input.size());
-
-  for(int i=input.size()-1; i>=0; i--) {
-          int curr = input[i];
-
-          //apka answer stack me 
-          while(s.top() != -1 && input[s.top()] >= curr) {
-                  s.pop();
-          }
-
-          //chotta element mil chuka h -> ans store
-          ans[i] = s.top();
-
-          //push krdo curr element ko
-          s.push(i);
-  }
-  return ans;
-}
-
     int largestRectangleArea(vector<int>& heights) {
-          //step1: prevSmaller output
-        vector<int> prev = prevSmallerElement(heights);
+         int n = heights.size();
+         stack<int>st;
+         int maxArea = 0;
 
-        //step2: nextSmaller call
-        vector<int> next = nextSmaller(heights);
+         for(int i=0;i<n;i++){
+             while(!st.empty() && heights[st.top()] > heights[i]){
+                  int element = st.top();
+                  st.pop();
 
-        int maxArea = INT_MIN;
-        int size = heights.size();
+                  //matlab iss particular bar ke liye abb mujhe nse mil gya hai 
+                  int nse = i;
+                  int pse = st.empty() ? -1 : st.top();
+                  int area = heights[element]*(nse - pse - 1);
+                  maxArea = max(maxArea , area);
+             }
+             st.push(i);
+         }
 
-        for(int i=0; i<heights.size(); i++) {
-                int length = heights[i];
-                
-                if(next[i] == -1) {
-                        next[i] = size;
-                }
-
-                int width = next[i] - prev[i] - 1;
-
-                int area = length * width;
-                maxArea = max(maxArea, area);
-        }
-
-        return maxArea;
-
+         // suppose after traversal some of the elemenst are still left 
+          while(!st.empty()){
+               int element = st.top();
+               st.pop();
+               int nse = n;
+               int pse = st.empty() ? -1 : st.top();
+               maxArea = max(maxArea , heights[element] * (nse - pse-1));
+          }
+          return maxArea;
     }
 };
