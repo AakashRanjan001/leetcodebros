899e2368cb701735ffd76edc895c6c7c67bc9a26
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-10-29 17:38:46 +0530

    Time: 319 ms (64.16%), Space: 0B (100%) - LeetHub

--- 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -1,11 +1,21 @@
 # Write your MySQL query statement below
 
-SELECT e1.employee_id
- FROM Employees  as e1 
-  LEFT JOIN 
-      Employees as e2
-      ON e1.manager_id = e2.employee_id
+-- SELECT e1.employee_id
+--  FROM Employees  as e1 
+--   LEFT JOIN 
+--       Employees as e2
+--       ON e1.manager_id = e2.employee_id
 
-   where e1.salary<30000 AND e1.manager_id is NOT NULL AND  e2.employee_id is NULL
-   ORDER BY e1.employee_id;
+--    where e1.salary<30000 AND e1.manager_id is NOT NULL AND  e2.employee_id is NULL
+--    ORDER BY e1.employee_id;
+
+SELECT employee_id
+FROM Employees
+Where salary<30000 AND
+  manager_id NOT IN (
+        SELECT employee_id
+        FROM Employees
+  )
+ 
+ ORDER BY employee_id;
   
