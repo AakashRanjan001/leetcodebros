435c634c00f41d0290ea9a740501202beb04e51b
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-11-04 13:07:02 +0530

    Time: 23 ms (61.48%), Space: 139.1 MB (58.39%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,55 +1,31 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        // ye sliding window plus queue ka question hao 
-         //brute force
-        //  int maxi = INT_MIN; 
-        //  vector<int>res;
-        //  for(int i=0;i<=nums.size() - k;i++){
-        //     maxi = INT_MIN;
-        //     for(int j=i;j<i+k;j++){
-        //           maxi = max(maxi,nums[j]);
-        //     }
-        //     res.push_back(maxi);
-        //  }
-        //  return res;
+         int n = nums.size();
+         deque<int>dq;
+         vector<int>res;
+         int i=0,j=0;
+         while(j<n){
+             //calculation 
+              while(!dq.empty() && dq.back()<nums[j]){
+                  dq.pop_back();
+              }
+              dq.push_back(nums[j]);
 
-       //optimal is sliding and queue
-       deque<int>q;
-       int n = nums.size();
-       int maxi = INT_MIN;
-       int i=0;
-       int j=0;
-       vector<int>res;
+              if(j-i+1<k)j++;
+              else if(j-i+1 == k){
+                 res.push_back(dq.front());
 
-
-       while(j<n){
-          //calculation to find out the maxi 
-            while( !q.empty() && q.back()<nums[j] ){
-                 q.pop_back();
-            }
-            q.push_back(nums[j]);
-
-            if(j-i+1 < k){
-                 j++;
-            }
-
-            else if(j-i+1 == k){
-                 //ans nikalo from calculationa and slide the window
-                 res.push_back(q.front());
-                 //slide 
-                 if(nums[i] == q.front()){
-                     q.pop_front();
-                    
+                 if(nums[i] == dq.front()){
+                     dq.pop_front();
+            
                  }
-                  i++;
-                  j++;
-                   
-            }
-           
-       }
-       return res;
-
+              i++;
+              j++;
+              }
+          
+         }
+         return res;
 
     }
 };
