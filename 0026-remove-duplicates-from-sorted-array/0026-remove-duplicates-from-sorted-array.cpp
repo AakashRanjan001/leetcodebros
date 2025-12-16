fdc91b69d7b68d0e90ed024d4a6d75af38044d89
Aakash Ranjan <144724018+AakashRanjan001@users.noreply.github.com>
2025-12-16 21:04:57 +0530

    Time: 0 ms (100%), Space: 22.5 MB (77.76%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        // means the error her withe the problem of set is that , we have to modify the array also , therefore we have to think it otherwise 
+
+        // it is a two pointer approach 
+        // i jo hai woh sirf unique element mei hi point karta hai 
+        // j jo hai woh unique element i ko laake dega 
+
         int n = nums.size();
-        if (n == 0) return 0;
+        int i=0,j=1;
 
-        int i = 0;
-        int j = 1;
-
-        while (j < n) {
-            if (nums[i] != nums[j]) {
-                i++;
-                nums[i] = nums[j];
-            }
-            j++;
+        while(j<n){
+            //  if(nums[i] == nums[j]){ j ko tab tak aage badhao jab tak nums[i]!=nums[j] na ho jjaye
+              if(nums[i] == nums[j]){
+                 j++;
+             }
+             else{ // j ko unique lement mila hai 
+                 i++;
+                 nums[i] = nums[j];
+                 j++;
+                 
+             }
         }
-
-        return i + 1;
+        return i+1;
+        
     }
 };
